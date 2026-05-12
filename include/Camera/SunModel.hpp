1ee20fab045fa29d97eeddf2e036af9bfe5d2525
ryanbevins <ryanabevins@gmail.com>
2026-05-13 06:12:43 +0800

    Decompile TSunModel::getZBufValue (74.6% match)
    Implements z-buffer sample loop checking 17 sun-glint points against
    GXPeekZ. Skips checks when indoor or coord == -1.
    Adds mZBufCoords[17][2] and mZBufVisible[17] to SunModel.hpp layout.

--- include/Camera/SunModel.hpp
+++ include/Camera/SunModel.hpp
@@ -10,10 +10,16 @@ public:
 	virtual void load(JSUMemoryInputStream&);
 	virtual void perform(u32, JDrama::TGraphics*);
 
+	void getZBufValue();
+
 public:
 	/* 0x10 */ u8 _10[0xAC - 0x10];
 	/* 0xAC */ f32 unkAC;
-	/* 0xB0 */ u8 _B0[0x1B0 - 0xB0];
+	/* 0xB0 */ u8 _B0[0xB4 - 0xB0];
+	/* 0xB4 */ s16 mZBufCoords[17][2];
+	/* 0xF8 */ u8 _F8[0x180 - 0xF8];
+	/* 0x180 */ u8 mZBufVisible[17];
+	/* 0x191 */ u8 _191[0x1B0 - 0x191];
 };
 
 extern TSunModel* gpSunModel;
--- src/Camera/sunmodel.cpp
+++ src/Camera/sunmodel.cpp
@@ -1,4 +1,6 @@
 #include <Camera/SunModel.hpp>
+#include <Camera/CameraMarioData.hpp>
+#include <dolphin/gx.h>
 
 TSunModel* gpSunModel;
 
@@ -13,7 +15,39 @@ TSunModel::TSunModel(bool sunset, const char* name)
 	for (u32 i = 0; i < sizeof(_B0); i++) {
 		_B0[i] = 0;
 	}
+	for (int i = 0; i < 17; i++) {
+		mZBufCoords[i][0] = -1;
+		mZBufCoords[i][1] = -1;
+		mZBufVisible[i]   = 0;
+	}
+	for (u32 i = 0; i < sizeof(_F8); i++) {
+		_F8[i] = 0;
+	}
+	for (u32 i = 0; i < sizeof(_191); i++) {
+		_191[i] = 0;
+	}
 	gpSunModel = this;
+}
+
+void TSunModel::getZBufValue()
+{
+	bool indoor = gpCameraMario->isMarioIndoor();
+	for (int i = 0; i < 17; i++) {
+		mZBufVisible[i] = 0;
+		if (indoor)
+			continue;
+		s16 x = mZBufCoords[i][0];
+		if (x == -1)
+			continue;
+		s16 y = mZBufCoords[i][1];
+		if (y == -1)
+			continue;
+		u32 zVal;
+		GXPeekZ(x, y, &zVal);
+		if ((zVal - 0xFF000000) == 0xFFFF) {
+			mZBufVisible[i] = 1;
+		}
+	}
 }
 
 void TSunModel::load(JSUMemoryInputStream& stream)
