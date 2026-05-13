ad560ac6accd9bcd64f2cf730d3689428560a0a1
ryanbevins <ryanabevins@gmail.com>
2026-05-13 09:31:27 +0800

    Decompile TMario::askJumpIntoWaterEffectExist (MATCH, 52 bytes)
    First function in MarioParticle.cpp.

--- src/Player/MarioParticle.cpp
+++ src/Player/MarioParticle.cpp
@@ -1,1 +1,12 @@
-// Player/MarioParticle.cpp — Mario particle effects. ~9KB. Stub for portability.
+#include <Player/MarioMain.hpp>
+#include <Player/MarioEffect.hpp>
+
+bool TMario::askJumpIntoWaterEffectExist() const
+{
+	TMarioEffect* eff = (TMarioEffect*)mMarioEffect;
+	if (eff->unk6C[0] == 1)
+		return true;
+	if (eff->unk6C[1] == 1)
+		return true;
+	return false;
+}
