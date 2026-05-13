cdea032cac2de83dfc1f500a865eaa691527a6f1
ryanbevins <ryanabevins@gmail.com>
2026-05-13 09:32:59 +0800

    Decompile TMario::sinkInSandEffect (MATCH, 92 bytes)
    Emit particle 0x53 and zero out emitter scale/dir fields.

--- src/Player/MarioParticle.cpp
+++ src/Player/MarioParticle.cpp
@@ -1,5 +1,7 @@
 #include <Player/MarioMain.hpp>
 #include <Player/MarioEffect.hpp>
+#include <System/EmitterViewObj.hpp>
+#include <JSystem/JParticle/JPAEmitter.hpp>
 
 bool TMario::askJumpIntoWaterEffectExist() const
 {
@@ -9,4 +11,18 @@ bool TMario::askJumpIntoWaterEffectExist() const
 	if (eff->unk6C[1] == 1)
 		return true;
 	return false;
+}
+
+void TMario::sinkInSandEffect()
+{
+	JPABaseEmitter* emitter
+	    = gpMarioParticleManager->emit(0x53, &mPosition, 0, nullptr);
+	if (emitter) {
+		emitter->unk154.x = 0.0f;
+		emitter->unk154.y = 0.0f;
+		emitter->unk154.z = 0.0f;
+		emitter->unk174.x = 0.0f;
+		emitter->unk174.y = 0.0f;
+		emitter->unk174.z = 0.0f;
+	}
 }
