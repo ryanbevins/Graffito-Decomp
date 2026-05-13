88981b2daca311f0fb73a34b7e43e459455392eb
ryanbevins <ryanabevins@gmail.com>
2026-05-13 09:41:54 +0800

    Decompile TMario blur effects + warpInLight (4 MATCHes)
    emitBlurSpinJump (228 bytes), emitBlurHipDropSuper (164 bytes),
    emitBlurHipDrop (68 bytes), warpInLight (60 bytes).

--- src/Player/MarioParticle.cpp
+++ src/Player/MarioParticle.cpp
@@ -42,3 +42,33 @@ void TMario::sleepingEffectKill()
 
 void TMario::emitSandEffect() { emitFootPrintWithEffect(0x3b, 0x3a); }
 void TMario::emitDirtyFootPrint() { emitFootPrintWithEffect(0x50, -1); }
+
+void TMario::emitBlurSpinJump()
+{
+	gpMarioParticleManager->emitAndBindToMtxPtr(0x105, getCenterAnmMtx(), 1, this);
+	gpMarioParticleManager->emitAndBindToMtxPtr(0x106, getCenterAnmMtx(), 1, this);
+	if (unk134 > 0.0f) {
+		JPABaseEmitter* emitter = gpMarioParticleManager->emitAndBindToMtxPtr(
+		    0x126, getCenterAnmMtx(), 1, this);
+		if (emitter)
+			emitter->mChildSpawnRate = 1.75f * unk134 * (1.0f / 255.0f);
+	}
+}
+
+void TMario::emitBlurHipDropSuper()
+{
+	gpMarioParticleManager->emitAndBindToMtxPtr(0x11A, getCenterAnmMtx(), 1, this);
+	gpMarioParticleManager->emitAndBindToMtxPtr(0x11B, getCenterAnmMtx(), 1, this);
+	gpMarioParticleManager->emitAndBindToMtxPtr(0x11C, getCenterAnmMtx(), 1, this);
+	gpMarioParticleManager->emitAndBindToMtxPtr(0x11D, getCenterAnmMtx(), 1, this);
+}
+
+void TMario::emitBlurHipDrop()
+{
+	gpMarioParticleManager->emitAndBindToMtxPtr(0x104, getCenterAnmMtx(), 1, this);
+}
+
+void TMario::warpInLight()
+{
+	gpMarioParticleManager->emitAndBindToPosPtr(0x51, &unk160[2], 0, this);
+}
