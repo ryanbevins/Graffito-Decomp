174f832587c69727f418470cf30939ade05d6569
ryanbevins <ryanabevins@gmail.com>
2026-05-16 11:46:53 +0800

    Decompile evIsDemoMode and evIsGameModeNormal SPC bindings (~88-92%)

--- src/NPC/NpcEvent.cpp
+++ src/NPC/NpcEvent.cpp
@@ -1,8 +1,30 @@
 #include <NPC/NpcEvent.hpp>
 #include <NPC/NpcBase.hpp>
 #include <System/FlagManager.hpp>
+#include <System/MarDirector.hpp>
+#include <Strategic/SpcInterp.hpp>
 #include <MSound/MSound.hpp>
 #include <MSound/MSoundSE.hpp>
+
+static void evIsDemoMode(TSpcTypedInterp<TEventWatcher>* interp, u32 arg_num)
+{
+	interp->verifyArgNum(0, &arg_num);
+	BOOL isDemo;
+	if (gpMarDirector->unk124 == 3) isDemo = TRUE;
+	else if (gpMarDirector->unk124 == 4) isDemo = TRUE;
+	else isDemo = FALSE;
+	interp->push(TSpcSlice((int)isDemo));
+}
+
+static void evIsGameModeNormal(TSpcTypedInterp<TEventWatcher>* interp,
+                               u32 arg_num)
+{
+	interp->verifyArgNum(0, &arg_num);
+	BOOL isNormal;
+	if (gpMarDirector->unk124 == 0) isNormal = TRUE;
+	else isNormal = FALSE;
+	interp->push(TSpcSlice((int)isNormal));
+}
 
 u32 TNpcEvent::mDownSunflowerNum;
 
