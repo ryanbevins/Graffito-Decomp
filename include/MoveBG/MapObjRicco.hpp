f7d56e48b53480dce6efe2a6e24b0440334ebf66
trader-vps <trader-vps@btc-predictor>
2026-05-25 15:42:03 +0000

    MoveBG/MapObjRicco: write TU 0 -> 87.57% (18/32 fns at 100%)
    7 classes implemented (TCraneRotY/UpDown/Cargo, TRiccoWatermill,
    TSurfGesoObj, TFruitSwitch, TFruitLauncher). Auto-generated dtors,
    sinit, simple calc/load functions all match. Remaining gaps are
    codegen polish on control/loadAfter/touchWater functions (75-85%).

--- include/MoveBG/MapObjRicco.hpp
+++ include/MoveBG/MapObjRicco.hpp
@@ -4,63 +4,145 @@
 #include <MoveBG/MapObjBase.hpp>
 #include <MoveBG/MapObjBlock.hpp>
 #include <MoveBG/Item.hpp>
-
-// TODO: mark virtual methods as such
+#include <dolphin/gx/GXStruct.h>
 
 class TCraneRotY : public TMapObjBase {
 public:
-	void calc();
-	void control();
-	void load(JSUMemoryInputStream&);
-	TCraneRotY();
+	TCraneRotY()
+	    : TMapObjBase("Ｙ軸回転クレーン")
+	    , unk138(0.0f)
+	    , unk13C(0.0f)
+	    , unk140(0.0f)
+	    , unk144(0.0f)
+	    , unk148(0)
+	{
+	}
+
+	virtual void load(JSUMemoryInputStream&);
+	virtual void control();
+	virtual void calc();
+
+	static s32 mWaitTime;
+
+public:
+	/* 0x138 */ f32 unk138;
+	/* 0x13C */ f32 unk13C;
+	/* 0x140 */ f32 unk140;
+	/* 0x144 */ f32 unk144;
+	/* 0x148 */ u32 unk148;
 };
 
 class TCraneUpDown : public TMapObjBase {
 public:
-	void control();
-	void initMapObj();
-	TCraneUpDown();
+	TCraneUpDown()
+	    : TMapObjBase("上下クレーン")
+	    , unk138(0)
+	    , unk13C(0)
+	    , unk140(0.0f)
+	    , unk144(0.0f)
+	{
+	}
+
+	virtual void initMapObj();
+	virtual void control();
+
+	static f32 mRotSpeed;
+	static s32 mWaitTime;
+
+public:
+	/* 0x138 */ TMapObjBase* unk138;
+	/* 0x13C */ u32 unk13C;
+	/* 0x140 */ f32 unk140;
+	/* 0x144 */ f32 unk144;
 };
 
 class TCraneCargo : public TLeanBlock {
 public:
-	void control();
-	void calc();
 	TCraneCargo()
 	    : TLeanBlock("クレーン積み荷")
 	{
 	}
+
+	virtual void control();
+	virtual void calc();
 };
 
 class TRiccoWatermill : public TMapObjBase {
 public:
-	u32 touchWater(THitActor*);
-	void control();
-	void calc();
-	void loadAfter();
 	TRiccoWatermill(const char*);
+
+	virtual void loadAfter();
+	virtual void control();
+	virtual void calc();
+	virtual u32 touchWater(THitActor*);
+
+	static f32 mRotAccel;
+	static f32 mRotSpeedMaxUp;
+	static f32 mRotSpeedMaxDown;
+	static f32 mRotDown;
+	static f32 mSubmarineMoveRate;
+	static f32 mSubmarineMaxTransY;
+	static f32 mSubmarineBottomTransY;
+	static f32 mSubmarineSurfaceTransY;
+	static s32 mWaitTime;
+
+public:
+	/* 0x138 */ f32 unk138;
+	/* 0x13C */ TMapObjBase* unk13C;
+	/* 0x140 */ u32 unk140;
+	/* 0x144 */ u8 unk144;
+	/* 0x148 */ TMapObjBase* unk148;
+	/* 0x14C */ void* unk14C;
+	/* 0x150 */ void* unk150;
+	/* 0x154 */ void* unk154;
 };
 
 class TSurfGesoObj : public TItem {
 public:
-	void initMapObj();
-	TSurfGesoObj();
+	TSurfGesoObj()
+	    : TItem("イカサーフィン")
+	{
+	}
+
+	virtual void initMapObj();
+
+public:
+	/* 0x154 */ GXColorS10 unk154;
 };
 
 class TFruitSwitch : public TMapObjBase {
 public:
-	void pullUp();
-	void pushDown();
-	BOOL receiveMessage(THitActor* sender, u32 message);
-	TFruitSwitch();
+	TFruitSwitch()
+	    : TMapObjBase("フルーツスイッチ")
+	    , unk138(0)
+	{
+	}
+
+	virtual BOOL receiveMessage(THitActor* sender, u32 message);
+
+public:
+	/* 0x138 */ TMapObjBase* unk138;
 };
 
 class TFruitLauncher : public TMapObjBase {
 public:
-	void appearFruit() const;
+	TFruitLauncher()
+	    : TMapObjBase("フルーツ発射")
+	    , unk140(0)
+	{
+	}
+
+	virtual void loadAfter();
 	void fireObj();
-	void loadAfter();
-	TFruitLauncher();
+
+	static f32 mObjSpeedXZ;
+	static f32 mObjSpeedY;
+	static s32 mFruitLiveTime;
+
+public:
+	/* 0x138 */ TFruitSwitch* unk138;
+	/* 0x13C */ TFruitSwitch* unk13C;
+	/* 0x140 */ u32 unk140;
 };
 
 #endif
--- src/MoveBG/MapObjRicco.cpp
+++ src/MoveBG/MapObjRicco.cpp
@@ -1,1 +1,579 @@
-// MoveBG/MapObjRicco.cpp — stub for portability (32 fns).
+#include <MoveBG/MapObjRicco.hpp>
+#include <MoveBG/MapObjBall.hpp>
+#include <MoveBG/MapObjManager.hpp>
+#include <MoveBG/ItemManager.hpp>
+#include <MoveBG/Item.hpp>
+#include <Map/MapCollisionManager.hpp>
+#include <Map/MapCollisionEntry.hpp>
+#include <Strategic/HitActor.hpp>
+#include <Strategic/LiveActor.hpp>
+#include <System/MarDirector.hpp>
+#include <System/EmitterViewObj.hpp>
+#include <System/Particles.hpp>
+#include <MSound/MSound.hpp>
+#include <MSound/MSoundSE.hpp>
+#include <M3DUtil/MActor.hpp>
+#include <M3DUtil/MActorUtil.hpp>
+#include <MarioUtil/RandomUtil.hpp>
+#include <JSystem/J3D/J3DGraphAnimator/J3DModel.hpp>
+#include <JSystem/JGeometry/JGUtil.hpp>
+#include <JSystem/JMath.hpp>
+#include <JSystem/JDrama/JDRNameRefGen.hpp>
+#include <dolphin/mtx.h>
+#include <string.h>
+
+#include <M3DUtil/InfectiousStrings.hpp>
+
+s32 TCraneRotY::mWaitTime = 60;
+f32 TCraneUpDown::mRotSpeed = 0.05f;
+s32 TCraneUpDown::mWaitTime = 60;
+f32 TRiccoWatermill::mRotAccel = 0.005f;
+f32 TRiccoWatermill::mRotSpeedMaxUp = 0.5f;
+f32 TRiccoWatermill::mRotSpeedMaxDown = 0.5f;
+f32 TRiccoWatermill::mRotDown = 0.01f;
+f32 TRiccoWatermill::mSubmarineMoveRate = 1.0f;
+f32 TRiccoWatermill::mSubmarineMaxTransY = 1500.0f;
+f32 TRiccoWatermill::mSubmarineBottomTransY = 0.0f;
+s32 TRiccoWatermill::mWaitTime = 120;
+f32 TRiccoWatermill::mSubmarineSurfaceTransY;
+f32 TFruitLauncher::mObjSpeedXZ = 5.0f;
+f32 TFruitLauncher::mObjSpeedY = 30.0f;
+s32 TFruitLauncher::mFruitLiveTime = 600;
+
+Vec submarineCranePos_forSound;
+
+void TCraneRotY::calc()
+{
+	setRootMtxRotY();
+}
+
+void TCraneRotY::control()
+{
+	TMapObjBase::control();
+	switch (mState) {
+	case 0:
+		mRotation.y += unk144;
+		if (mRotation.y > unk138 + unk140) {
+			mLifeTimer = mWaitTime;
+			mState     = 3;
+		}
+		break;
+	case 1:
+		if (mLifeTimer <= 0) {
+			mState = 2;
+		}
+		break;
+	case 2:
+		mRotation.y -= unk144;
+		if (mRotation.y < unk138 + unk13C) {
+			mLifeTimer = mWaitTime;
+			mState     = 1;
+		}
+		break;
+	case 3:
+		if (mLifeTimer <= 0) {
+			mState = 0;
+		}
+		break;
+	}
+	if (mState == 0 || mState == 2) {
+		u32 sound = unk148;
+		if (gpMSound->gateCheck(sound)) {
+			MSoundSESystem::MSoundSE::startSoundActor(
+			    sound, (Vec*)&mPosition, 0, 0, 0, 4);
+		}
+	}
+}
+
+void TCraneRotY::load(JSUMemoryInputStream& stream)
+{
+	TMapObjBase::load(stream);
+	stream.read(&unk140, 4);
+	unk138 = mRotation.y;
+	unk144 = 0.5f + MsRandF() * 0.3f;
+	if (strcmp(unkF4, "crane90 0") == 0) {
+		unk148 = 0x3034;
+	} else {
+		unk148 = 0x3035;
+	}
+	mState = 0;
+}
+
+void TCraneUpDown::control()
+{
+	TMapObjBase::control();
+	switch (mState) {
+	case 0:
+		mRotation.x += mRotSpeed;
+		if (mRotation.x > unk140) {
+			mLifeTimer = mWaitTime;
+			mState     = 3;
+		}
+		break;
+	case 1:
+		if (mLifeTimer <= 0) {
+			mState = 0;
+		}
+		break;
+	case 2:
+		mRotation.x -= mRotSpeed;
+		if (mRotation.x < unk144) {
+			mLifeTimer = mWaitTime;
+			mState     = 1;
+		}
+		break;
+	case 3:
+		if (mLifeTimer <= 0) {
+			mState = 2;
+		}
+		break;
+	}
+	{
+		JGeometry::TVec3<f32>* pos = (JGeometry::TVec3<f32>*)&unk138->mPosition;
+		pos->x                     = 0.0f;
+		pos->y                     = 0.0f;
+		pos->z                     = 100.0f;
+		Mtx scratch;
+		PSMTXIdentity(scratch);
+		s16 angY  = (s16)(mRotation.x * 182.04445f);
+		f32 sinY  = jmaSinTable[(u16)angY >> jmaSinShift];
+		f32 cosY  = jmaCosTable[(u16)angY >> jmaSinShift];
+		Mtx rotY;
+		rotY[0][0] = 1.0f;
+		rotY[0][1] = 0.0f;
+		rotY[0][2] = 0.0f;
+		rotY[0][3] = 0.0f;
+		rotY[1][0] = 0.0f;
+		rotY[1][1] = cosY;
+		rotY[1][2] = -sinY;
+		rotY[1][3] = 0.0f;
+		rotY[2][0] = 0.0f;
+		rotY[2][1] = sinY;
+		rotY[2][2] = cosY;
+		rotY[2][3] = 0.0f;
+		s16 angZ   = (s16)(mRotation.y * 182.04445f);
+		f32 sinZ   = jmaSinTable[(u16)angZ >> jmaSinShift];
+		f32 cosZ   = jmaCosTable[(u16)angZ >> jmaSinShift];
+		scratch[0][0] = cosZ;
+		scratch[0][1] = 0.0f;
+		scratch[0][2] = sinZ;
+		scratch[0][3] = 0.0f;
+		scratch[1][0] = 0.0f;
+		scratch[1][1] = 1.0f;
+		scratch[1][2] = 0.0f;
+		scratch[1][3] = 0.0f;
+		scratch[2][0] = -sinZ;
+		scratch[2][1] = 0.0f;
+		scratch[2][2] = cosZ;
+		scratch[2][3] = 0.0f;
+		PSMTXConcat(rotY, scratch, rotY);
+		PSMTXMultVec(rotY, (Vec*)&unk138->mPosition, (Vec*)&unk138->mPosition);
+		unk138->mPosition.x += mPosition.x;
+		unk138->mPosition.y += mPosition.y - mYOffset + unk138->mYOffset;
+		unk138->mPosition.z += mPosition.z;
+	}
+	if (mState == 0 || mState == 2) {
+		u32 sound = unk13C;
+		if (gpMSound->gateCheck(sound)) {
+			MSoundSESystem::MSoundSE::startSoundActor(
+			    sound, (Vec*)&mPosition, 0, 0, 0, 4);
+		}
+	}
+}
+
+void TCraneUpDown::initMapObj()
+{
+	TMapObjBase::initMapObj();
+	mMapCollisionManager->unk8->setAllActor(0);
+	unk138 = TMapObjBaseManager::newAndRegisterObj(
+	    "craneCargoUpDown",
+	    JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	    JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	    JGeometry::TVec3<f32>(1.0f, 1.0f, 1.0f));
+	(*(void (**)(TMapObjBase*))((*(void***)unk138)[0xFC / 4]))(unk138);
+	if (strcmp(unkF4, "craneUpDown 0") == 0) {
+		unk144 = 30.0f;
+		unk140 = -30.0f;
+		unk13C = 0x3036;
+	} else {
+		unk144 = 30.0f;
+		unk140 = -60.0f;
+		unk13C = 0x3037;
+	}
+	mRotation.x = unk144 + (unk140 - unk144) * MsRandF();
+}
+
+void TCraneCargo::control()
+{
+	unk158.set(0.0f, 0.0f, 0.0f);
+	TMapObjBase::control();
+}
+
+void TCraneCargo::calc()
+{
+	updateRootMtxTrans();
+	calcLeanMtx((MtxPtr)((u8*)getModel()->mNodeMatrices + 0x30));
+}
+
+u32 TRiccoWatermill::touchWater(THitActor* sender)
+{
+	if (mState == 5) {
+		return 1;
+	}
+	unk140 = 5;
+	if (mState == 1) {
+		unk13C->setUpMapCollision(1);
+	}
+	unkF8 &= ~0x100;
+	unk13C->unkF8 &= ~0x100;
+	if (unk13C->mPosition.y < mSubmarineMaxTransY) {
+		unk138 += mRotAccel;
+		if (unk138 > mRotSpeedMaxUp) {
+			unk138 = mRotSpeedMaxUp;
+		}
+		mState = 2;
+	} else {
+		unk138 = 0.0f;
+	}
+	return 1;
+}
+
+void TRiccoWatermill::control()
+{
+	TMapObjBase::control();
+	if (mState == 2 || mState == 3 || mState == 4) {
+		if (0.0f != unk138) {
+			mRotation.z -= unk138;
+			if (gpMSound->gateCheck(0x3031)) {
+				MSoundSESystem::MSoundSE::startSoundActorWithInfo(
+				    0x3031, (Vec*)&mPosition, (Vec*)NULL, __fabsf(unk138), 0, 0,
+				    (JAISound**)&unk14C, 0, 4);
+			}
+			f32 rate = unk138 * mSubmarineMoveRate;
+			unk13C->mPosition.y += rate;
+			if (gpMSound->gateCheck(0x3030)) {
+				MSoundSESystem::MSoundSE::startSoundActorWithInfo(
+				    0x3030, &submarineCranePos_forSound, (Vec*)NULL,
+				    __fabsf(rate), 0, 0, (JAISound**)&unk150, 0, 4);
+			}
+			if (gpMSound->gateCheck(0x3023)) {
+				MSoundSESystem::MSoundSE::startSoundActorWithInfo(
+				    0x3023, &submarineCranePos_forSound, (Vec*)NULL,
+				    __fabsf(rate), 0, 0, (JAISound**)&unk154, 0, 4);
+			}
+		}
+		if (unk140 == 0) {
+			unk138 -= mRotDown;
+			if (unk138 < -mRotSpeedMaxDown) {
+				unk138 = -mRotSpeedMaxDown;
+			}
+		} else {
+			unk140--;
+		}
+	}
+	switch (mState) {
+	case 2:
+		if (unk13C->mPosition.y > mSubmarineMaxTransY) {
+			unk13C->mPosition.y = mSubmarineMaxTransY;
+			if (!(mLifeTimer > 0) && !unk144) {
+				if (gpMSound->gateCheck(0x3832)) {
+					MSoundSESystem::MSoundSE::startSoundActor(
+					    0x3832, (Vec*)&unk13C->mPosition, 0, 0, 0, 4);
+				}
+				JGeometry::TVec3<f32> target(50.0f,
+				                             mSubmarineMaxTransY + 200.0f,
+				                             100.0f);
+				throwObjToFrontFromPoint(unk148, target, 0.0f, 0.0f);
+				unk144 = 1;
+			}
+			if (unk138 < 0.0f) {
+				if (unk144) {
+					mState = 3;
+				} else {
+					mState = 4;
+				}
+			}
+		}
+		break;
+	case 3:
+		if (unk13C->mPosition.y <= mSubmarineSurfaceTransY) {
+			unk13C->mPosition.y = mSubmarineSurfaceTransY;
+			unk13C->setUpMapCollision(0);
+			unk138 = 0.0f;
+			if (gpMSound->gateCheck(0x3832)) {
+				MSoundSESystem::MSoundSE::startSoundActor(
+				    0x3832, (Vec*)&unk13C->mPosition, 0, 0, 0, 4);
+			}
+			mLifeTimer = mWaitTime;
+			mState     = 5;
+		}
+		break;
+	case 4:
+		if (unk13C->mPosition.y <= mSubmarineBottomTransY) {
+			unk13C->mPosition.y = mSubmarineBottomTransY;
+			unk13C->setUpMapCollision(0);
+			unk138 = 0.0f;
+			unk13C->unkF8 |= 0x100;
+			unkF8 |= 0x100;
+			if (gpMSound->gateCheck(0x3833)) {
+				MSoundSESystem::MSoundSE::startSoundActor(
+				    0x3833, (Vec*)&unk13C->mPosition, 0, 0, 0, 4);
+			}
+			mState = 1;
+		}
+		break;
+	}
+}
+
+void TRiccoWatermill::calc()
+{
+	setRootMtxRotZ();
+}
+
+void TRiccoWatermill::loadAfter()
+{
+	TMapObjBase::loadAfter();
+	unk13C = (TMapObjBase*)JDrama::TNameRefGen::instance->mRootNameRef->searchF(
+	    JDrama::TNameRef::calcKeyCode("submarine"), "submarine");
+	unk148 = (TMapObjBase*)JDrama::TNameRefGen::instance->mRootNameRef->searchF(
+	    JDrama::TNameRef::calcKeyCode("水中スイッチ・押す擬似"),
+	    "水中スイッチ・押す擬似");
+	(*(void (**)(TMapObjBase*))((*(void***)unk148)[0x104 / 4]))(unk148);
+	unk13C->mPosition.y = mSubmarineBottomTransY;
+	unk13C->removeMapCollision();
+	(*(void (**)(TMapObjBase*))((*(void***)unk13C)[0x114 / 4]))(unk13C);
+}
+
+TRiccoWatermill::TRiccoWatermill(const char* name)
+    : TMapObjBase(name)
+{
+	unk138 = 0.0f;
+	unk13C = 0;
+	unk140 = 0;
+	unk144 = 0;
+	unk148 = 0;
+	unk14C = 0;
+	unk150 = 0;
+	unk154 = 0;
+}
+
+void TSurfGesoObj::initMapObj()
+{
+	TMapObjBase::initMapObj();
+	if (strcmp(unkF4, "SurfGesoRed") == 0) {
+		unk154.r = 0xFF;
+		unk154.g = 0xB4;
+		unk154.b = 0xFF;
+		unk154.a = 0xFF;
+	} else if (strcmp(unkF4, "SurfGesoYellow") == 0) {
+		unk154.r = 0xFF;
+		unk154.g = 0xFF;
+		unk154.b = 0x7D;
+		unk154.a = 0xFF;
+	} else if (strcmp(unkF4, "SurfGesoGreen") == 0) {
+		unk154.r = 0xB4;
+		unk154.g = 0xFF;
+		unk154.b = 0xB4;
+		unk154.a = 0xFF;
+	}
+	SDLModelData* modelData = gpMapObjManager->mSurfGessoModelData;
+	mMActor                 = SMS_MakeMActorFromSDLModelData(
+        modelData, gpMapObjManager->getMActorAnmData(), 3);
+	initPacketMatColor(getModel(), GX_TEVREG2, &unk154);
+	mMActor->setBck("surfgeso_run1");
+}
+
+BOOL TFruitSwitch::receiveMessage(THitActor* sender, u32 message)
+{
+	if (message == 1) {
+		startBck("riccoswitch");
+		unk64 |= 1;
+		TMapCollisionBase* coll = mMapCollisionManager->unk8;
+		if (coll) {
+			coll->remove();
+		}
+		((TFruitLauncher*)unk138)->fireObj();
+		return TRUE;
+	}
+	return FALSE;
+}
+
+static void fireFruitOnce(TMapObjBase* sw, u32* result)
+{
+	(void)sw;
+	(void)result;
+}
+
+void TFruitLauncher::fireObj()
+{
+	gpMarioParticleManager->emitAndBindToPosPtr(
+	    0x11, (JGeometry::TVec3<f32>*)&mPosition, 0, 0);
+	if (gpMSound->gateCheck(0x384C)) {
+		MSoundSESystem::MSoundSE::startSoundActor(
+		    0x384C, (Vec*)&mPosition, 0, 0, 0, 4);
+	}
+	if (gpMSound->gateCheck(0x387D)) {
+		MSoundSESystem::MSoundSE::startSoundActor(
+		    0x387D, (Vec*)&mPosition, 0, 0, 0, 4);
+	}
+	unk140              = unk140 == 0 ? 1 : 0;
+	TFruitSwitch* sw    = (&unk138)[unk140];
+	sw->getMActor()->getFrameCtrl(0)->setFrame(0.0f);
+	sw->unk64 &= ~1;
+	sw->getModel()->entry();
+	PSMTXCopy(sw->getModel()->mNodeMatrices[0],
+	          sw->mMapCollisionManager->unk8->unk20);
+	sw->mMapCollisionManager->unk8->setUp();
+
+	TMapObjBase* obj = (TMapObjBase*)NULL;
+	{
+		f32 r1 = MsRandF() * 5.0f;
+		if (r1 < 1.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000390,
+			                                   false);
+		} else if (r1 < 2.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000391,
+			                                   false);
+		} else if (r1 < 3.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000392,
+			                                   false);
+		} else if (r1 < 4.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000393,
+			                                   false);
+		} else {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000394,
+			                                   false);
+		}
+	}
+	if (obj == NULL) {
+		f32 r2 = MsRandF() * 5.0f;
+		if (r2 < 1.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000390,
+			                                   false);
+		} else if (r2 < 2.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000391,
+			                                   false);
+		} else if (r2 < 3.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000392,
+			                                   false);
+		} else if (r2 < 4.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000393,
+			                                   false);
+		} else {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000394,
+			                                   false);
+		}
+	}
+	if (obj == NULL) {
+		f32 r3 = MsRandF() * 5.0f;
+		if (r3 < 1.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000390,
+			                                   false);
+		} else if (r3 < 2.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000391,
+			                                   false);
+		} else if (r3 < 3.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000392,
+			                                   false);
+		} else if (r3 < 4.0f) {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000393,
+			                                   false);
+		} else {
+			obj = gpItemManager->makeObjAppear(sw->mPosition.x, sw->mPosition.y,
+			                                   sw->mPosition.z, 0x4000394,
+			                                   false);
+		}
+	}
+	if (obj != NULL) {
+		obj->mPosition.x = sw->mPosition.x;
+		obj->mPosition.y = sw->mPosition.y;
+		obj->mPosition.z = sw->mPosition.z;
+		f32 angle        = MsRandF() * 6.2831855f - 3.1415927f;
+		f32 speedXZ      = mObjSpeedXZ;
+		f32 speedY       = -mObjSpeedY;
+		obj->mLinearVelocity.x = speedXZ * sinf(angle);
+		obj->mLinearVelocity.y = speedY;
+		obj->mLinearVelocity.z = speedXZ * cosf(angle);
+		obj->unkF8 &= ~0x10;
+		gpMarDirector->fireStartDemoCamera(
+		    "フルーツスイッチメラメラ",
+		    (JGeometry::TVec3<f32>*)&obj->mPosition, -1, 0.0f, true,
+		    (s32 (*)(u32, u32))NULL, 0, (JDrama::TActor*)NULL,
+		    JDrama::TFlagT<u16>(0));
+		if (gpMSound->gateCheck(0x4849)) {
+			MSoundSESystem::MSoundSE::startSoundSystemSE(0x4849, 0,
+			                                             (JAISound**)NULL, 0);
+		}
+		if (isFruit(obj)) {
+			((TResetFruit*)obj)->killByTimer(mFruitLiveTime);
+		}
+	}
+}
+
+void TFruitLauncher::loadAfter()
+{
+	TMapObjBase::loadAfter();
+	((TResetFruit*)TMapObjBaseManager::newAndRegisterObj(
+	     "FruitCoconut", JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(1.0f, 1.0f, 1.0f)))
+	    ->unk1A4
+	    = 1;
+	((TResetFruit*)TMapObjBaseManager::newAndRegisterObj(
+	     "FruitDurian", JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(1.0f, 1.0f, 1.0f)))
+	    ->unk1A4
+	    = 1;
+	((TResetFruit*)TMapObjBaseManager::newAndRegisterObj(
+	     "FruitPapaya", JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(1.0f, 1.0f, 1.0f)))
+	    ->unk1A4
+	    = 1;
+	((TResetFruit*)TMapObjBaseManager::newAndRegisterObj(
+	     "FruitPine", JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(1.0f, 1.0f, 1.0f)))
+	    ->unk1A4
+	    = 1;
+	((TResetFruit*)TMapObjBaseManager::newAndRegisterObj(
+	     "FruitBanana", JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(0.0f, 0.0f, 0.0f),
+	     JGeometry::TVec3<f32>(1.0f, 1.0f, 1.0f)))
+	    ->unk1A4
+	    = 1;
+
+	unk138 = (TFruitSwitch*)JDrama::TNameRefGen::instance->mRootNameRef->searchF(
+	    JDrama::TNameRef::calcKeyCode("テレサスイッチ0"), "テレサスイッチ0");
+	unk138->unk138 = (TMapObjBase*)this;
+	unk13C = (TFruitSwitch*)JDrama::TNameRefGen::instance->mRootNameRef->searchF(
+	    JDrama::TNameRef::calcKeyCode("テレサスイッチ1"), "テレサスイッチ1");
+	unk13C->unk138 = (TMapObjBase*)this;
+	unk140         = 1;
+	unk138->startBck("@1490");
+	unk138->unk64 |= 1;
+	{
+		TMapCollisionBase* coll = unk138->mMapCollisionManager->unk8;
+		if (coll) {
+			coll->remove();
+		}
+	}
+}
+
+#include <MSound/MSoundBGM.hpp>
+#include <MSound/MSSetSound.hpp>
