ce23f834c3e6c2e8f4afcddb68a5e885fb8b89de
trader-vps <trader-vps@btc-predictor>
2026-05-26 11:25:46 +0000

    Camera/sunmodel: scaffold (7.5 -> 35.9%)
    Add TActor inheritance + typed fields at 0x44-0x1B0; reorder source
    top-to-bottom = bottom-to-top of map for -inline deferred TU.
    calcOtherFPosFromCenterAndRadius_ 0->100, perform 0.4->70, __dt 90->100,
    plus 3 helper symbols auto-emitted at 100.

--- include/Camera/SunModel.hpp
+++ include/Camera/SunModel.hpp
@@ -1,9 +1,17 @@
 #ifndef CAMERA_SUN_MODEL_HPP
 #define CAMERA_SUN_MODEL_HPP
 
-#include <JSystem/JDrama/JDRViewObj.hpp>
+#include <JSystem/JDrama/JDRActor.hpp>
+#include <JSystem/J3D/J3DGraphAnimator/J3DAnimation.hpp>
+#include <JSystem/JGeometry.hpp>
+#include <dolphin/types.h>
 
-class TSunModel : public JDrama::TViewObj {
+class J3DModel;
+class J3DModelData;
+class J3DAnmTextureSRTKey;
+class TMapStaticObj;
+
+class TSunModel : public JDrama::TActor {
 public:
 	TSunModel(bool, const char*);
 
@@ -11,17 +19,50 @@ public:
 	virtual void perform(u32, JDrama::TGraphics*);
 
 	void getZBufValue();
+	void calcDispRatioAndScreenPos_();
+	void calcOtherFPosFromCenterAndRadius_(JGeometry::TVec2<f32>* out,
+	                                       const JGeometry::TVec2<f32>& center,
+	                                       f32 radius);
 
 public:
-	/* 0x10 */ u8 _10[0xAC - 0x10];
-	/* 0xAC */ f32 unkAC;
-	/* 0xB0 */ u8 _B0[0xB4 - 0xB0];
-	/* 0xB4 */ s16 mZBufCoords[17][2];
-	/* 0xF8 */ f32 unkF8;
-	/* 0xFC */ f32 unkFC;
-	/* 0x100 */ u8 _100[0x180 - 0x100];
-	/* 0x180 */ u8 mZBufVisible[17];
-	/* 0x191 */ u8 _191[0x1B0 - 0x191];
+	/* 0x44 */ J3DModelData*       mModelData;
+	/* 0x48 */ J3DModel*           mModel;
+	/* 0x4C */ J3DAnmTextureSRTKey* mAnmTexSRT;
+	/* 0x50 */ J3DFrameCtrl        mFrameCtrl;
+	/* 0x64 */ TMapStaticObj*      mMapStaticObj;
+	/* 0x68 */ u8                  mUnk68;
+	/* 0x69 */ u8                  _69[3];
+	/* 0x6C */ f32                 mUnk6C;
+	/* 0x70 */ f32                 mUnk70;
+	/* 0x74 */ u8                  mUnk74;
+	/* 0x75 */ u8                  _75[3];
+	/* 0x78 */ f32                 mUnk78;
+	/* 0x7C */ f32                 mUnk7C;
+	/* 0x80 */ u8                  mUnk80;
+	/* 0x81 */ u8                  _81[3];
+	/* 0x84 */ f32                 mUnk84;
+	/* 0x88 */ f32                 mUnk88;
+	/* 0x8C */ u8                  _8C[0x9C - 0x8C];
+	/* 0x9C */ f32                 mUnk9C;
+	/* 0xA0 */ f32                 mUnkA0;
+	/* 0xA4 */ f32                 mUnkA4;
+	/* 0xA8 */ f32                 mUnkA8;
+	/* 0xAC */ f32                 unkAC;
+	/* 0xB0 */ f32                 mUnkB0;
+	/* 0xB4 */ JGeometry::TVec2<s16> mZBufCoords[17];
+	/* 0xF8 */ f32                 unkF8;
+	/* 0xFC */ f32                 unkFC;
+	/* 0x100 */ JGeometry::TVec2<f32> mInnerCircle[8];
+	/* 0x140 */ JGeometry::TVec2<f32> mOuterCircle[8];
+	/* 0x180 */ u8                 mZBufVisible[17];
+	/* 0x191 */ u8                 mVisibleCount;
+	/* 0x192 */ u8                 _192[2];
+	/* 0x194 */ f32                mUnk194;
+	/* 0x198 */ Vec                mPos198;
+	/* 0x1A4 */ f32                mUnk1A4;
+	/* 0x1A8 */ f32                mUnk1A8;
+	/* 0x1AC */ u8                 mFlags;
+	/* 0x1AD */ u8                 _1AD[3];
 };
 
 extern TSunModel* gpSunModel;
--- src/Camera/sunmodel.cpp
+++ src/Camera/sunmodel.cpp
@@ -1,9 +1,86 @@
 #include <Camera/SunModel.hpp>
+#include <Camera/Camera.hpp>
 #include <Camera/CameraMarioData.hpp>
 #include <Camera/cameralib.hpp>
+#include <MarioUtil/MathUtil.hpp>
 #include <System/Resolution.hpp>
 #include <JSystem/JGeometry.hpp>
 #include <dolphin/gx.h>
+#include <dolphin/mtx.h>
+
+TSunModel* gpSunModel;
+
+TSunModel::TSunModel(bool sunset, const char* name)
+    : JDrama::TActor(name)
+{
+	mModelData = 0;
+	mModel     = 0;
+	mAnmTexSRT = 0;
+	mFrameCtrl.init(0);
+	mMapStaticObj = 0;
+	mUnk68 = 0xFF;
+	mUnk6C = 0.1f;
+	mUnk70 = 0.01f;
+	mUnk74 = 50;
+	mUnk78 = 0.1f;
+	mUnk7C = 0.01f;
+	mUnk80 = 100;
+	mUnk84 = 3.0f;
+	mUnk88 = 2.0f;
+	mUnk9C = 0.0f;
+	mUnkA0 = 0.0f;
+	mUnkA4 = 0.0f;
+	mUnkA8 = 0.0f;
+	unkAC  = 0.0f;
+	mUnkB0 = 0.0f;
+	mVisibleCount = 0;
+	mUnk194 = 0.0f;
+	mUnk1A4 = 0.014f;
+	mUnk1A8 = 3.0f;
+	mFlags  = 0;
+	gpSunModel = this;
+	if (sunset) {
+		mFlags |= 4;
+		mUnk80 = 0x30;
+	}
+
+	for (int i = 0; i < 17; i++) {
+		mZBufCoords[i].y = -1;
+		mZBufCoords[i].x = -1;
+		mInnerCircle[i % 8].x = 0.0f; // these arrays are init'd by ctors; the
+		mInnerCircle[i % 8].y = 0.0f; // separate loop on mZBufVisible below
+		mZBufVisible[i]       = 0;
+	}
+}
+
+void TSunModel::load(JSUMemoryInputStream& stream)
+{
+	JDrama::TActor::load(stream);
+	// TODO: full body
+	(void)stream;
+}
+
+void TSunModel::calcOtherFPosFromCenterAndRadius_(
+    JGeometry::TVec2<f32>* out, const JGeometry::TVec2<f32>& center, f32 radius)
+{
+	f32 d = 0.707f * radius;
+	out[0].x = center.x;
+	out[0].y = center.y + radius;
+	out[1].x = center.x - d;
+	out[1].y = center.y + d;
+	out[2].x = center.x - radius;
+	out[2].y = center.y;
+	out[3].x = center.x - d;
+	out[3].y = center.y - d;
+	out[4].x = center.x;
+	out[4].y = center.y - radius;
+	out[5].x = center.x + d;
+	out[5].y = center.y - d;
+	out[6].x = center.x + radius;
+	out[6].y = center.y;
+	out[7].x = center.x + d;
+	out[7].y = center.y + d;
+}
 
 template <class S, class F>
 void CLBScreenFPosToSPos(JGeometry::TVec2<S>* dst, const JGeometry::TVec2<F>& src)
@@ -25,34 +102,130 @@ void CLBScreenFPosToSPos(JGeometry::TVec2<S>* dst, const JGeometry::TVec2<F>& sr
 template void CLBScreenFPosToSPos<s16, f32>(JGeometry::TVec2<s16>*,
                                             const JGeometry::TVec2<f32>&);
 
-
-TSunModel* gpSunModel;
-
-TSunModel::TSunModel(bool sunset, const char* name)
-    : JDrama::TViewObj(name)
+void TSunModel::calcDispRatioAndScreenPos_()
 {
-	(void)sunset;
-	for (u32 i = 0; i < sizeof(_10); i++) {
-		_10[i] = 0;
+	mVisibleCount = 0;
+	for (s32 i = 0; i < 17; i++) {
+		if (mZBufVisible[i] != 0) {
+			mVisibleCount += 1;
+		}
 	}
-	unkAC = 0.0f;
-	for (u32 i = 0; i < sizeof(_B0); i++) {
-		_B0[i] = 0;
+	mUnk194 = 0.05882353f * (f32)(u8)mVisibleCount;
+
+	CLBCalc2DFPos(
+	    (JGeometry::TVec2<f32>*)&unkF8,
+	    (MtxPtr)((u8*)gpCamera + 0x16C),
+	    (MtxPtr)((u8*)gpCamera + 0x1EC),
+	    mPos198, 0, false);
+
+	f32 inner = mUnk1A4 * mScaling.x;
+	calcOtherFPosFromCenterAndRadius_(mInnerCircle,
+	                                  *(JGeometry::TVec2<f32>*)&unkF8, inner);
+	f32 outer = inner * 0.5f;
+	calcOtherFPosFromCenterAndRadius_(mOuterCircle,
+	                                  *(JGeometry::TVec2<f32>*)&unkF8, outer);
+
+	for (s32 i = 0; i < 17; i++) {
+		CLBScreenFPosToSPos<s16, f32>(
+		    &mZBufCoords[i],
+		    *(JGeometry::TVec2<f32>*)((u8*)&unkF8 + i * 8));
 	}
-	for (int i = 0; i < 17; i++) {
-		mZBufCoords[i][0] = -1;
-		mZBufCoords[i][1] = -1;
-		mZBufVisible[i]   = 0;
+}
+
+void TSunModel::perform(u32 flags, JDrama::TGraphics* gfx)
+{
+	(void)gfx;
+	bool inMode = false;
+	if (gpCameraMario->isMarioIndoor()) {
+		inMode = false;
+	} else {
+		bool a = false, b = false, c = false;
+		if (-mUnk1A8 <= unkF8 && unkF8 <= mUnk1A8) {
+			a = true;
+		}
+		if (a && -mUnk1A8 <= unkFC) {
+			b = true;
+		}
+		if (b && unkFC <= mUnk1A8) {
+			c = true;
+		}
+		inMode = (c ? true : false);
 	}
-	unkF8 = 0.0f;
-	unkFC = 0.0f;
-	for (u32 i = 0; i < sizeof(_100); i++) {
-		_100[i] = 0;
+
+	if ((flags & 1) != 0) {
+		mUnkA4 = CLBLinearInbetween<f32>((f32)mUnk68, 255.0f, mUnk194);
+		mUnkA8 = CLBEaseOutInbetween<f32>((f32)mUnk74, 255.0f, mUnk194);
+
+		f32 speed;
+		if (mUnk9C < mUnkA4) {
+			speed = mUnk6C;
+		} else {
+			speed = mUnk70;
+		}
+		CLBChaseDecrease(&mUnk9C, mUnkA4, 0.5f, speed);
+		mZBufCoords[0].y = (s16)(s32)mUnk9C;
+
+		if (mUnkA0 < mUnkA8) {
+			speed = mUnk78;
+		} else {
+			speed = mUnk7C;
+		}
+		CLBChaseDecrease(&mUnkA0, mUnkA8, 0.5f, speed);
+		mZBufCoords[0].x = (s16)(s32)mUnkA0;
+
+		if (gpCameraMario->isMarioIndoor()) {
+			mUnkB0 = 0.0f;
+		} else {
+			f32 d2 = unkF8 * unkF8 + unkFC * unkFC;
+			if (d2 > 2.0f) {
+				mUnkB0 = 0.0f;
+			} else {
+				mUnkB0 = CLBLinearInbetween<f32>(
+				    0.0f, 0.5f * (2.0f - d2) * 3.0f, (f32)mUnk80);
+			}
+		}
+
+		f32 chase_speed;
+		if (unkAC < mUnkB0) {
+			chase_speed = mUnk84;
+		} else {
+			chase_speed = mUnk88;
+		}
+		CLBChaseGeneralConstantSpecifySpeed<f32>(&unkAC, mUnkB0, chase_speed);
+
+		f32* gpcam = (f32*)((u8*)gpCamera + 0x124);
+		Vec axis;
+		axis.x = mPosition.x - gpcam[0];
+		axis.y = mPosition.y - gpcam[1];
+		axis.z = mPosition.z - gpcam[2];
+		MsVECNormalize(&axis, &axis);
+
+		JGeometry::TVec3<f32> cam_pos;
+		cam_pos.set(*(const Vec*)((u8*)gpCamera + 0x124));
+
+		mPos198.x = axis.x * 250000.0f + cam_pos.x;
+		mPos198.y = axis.y * 250000.0f + cam_pos.y;
+		mPos198.z = axis.z * 250000.0f + cam_pos.z;
+
+		if (mMapStaticObj != NULL) {
+			*(Vec*)((u8*)mMapStaticObj + 0x10) = mPos198;
+		}
+
+		calcDispRatioAndScreenPos_();
 	}
-	for (u32 i = 0; i < sizeof(_191); i++) {
-		_191[i] = 0;
+
+	if ((flags & 2) != 0) {
+		mFrameCtrl.update();
+		if (inMode) {
+			Mtx mtx;
+			MsMtxSetTRS(mtx, mPos198.x, mPos198.y, mPos198.z,
+			            mRotation.x, mRotation.y, mRotation.z,
+			            mScaling.x, mUnk9C * 0.0f, mUnk9C * 0.0f);
+			(void)mtx;
+			// stub draw path
+		}
 	}
-	gpSunModel = this;
+	(void)flags;
 }
 
 void TSunModel::getZBufValue()
@@ -62,10 +235,10 @@ void TSunModel::getZBufValue()
 		mZBufVisible[i] = 0;
 		if (indoor)
 			continue;
-		s16 x = mZBufCoords[i][0];
+		s16 x = mZBufCoords[i].x;
 		if (x == -1)
 			continue;
-		s16 y = mZBufCoords[i][1];
+		s16 y = mZBufCoords[i].y;
 		if (y == -1)
 			continue;
 		u32 zVal;
@@ -74,17 +247,4 @@ void TSunModel::getZBufValue()
 			mZBufVisible[i] = 1;
 		}
 	}
-}
-
-void TSunModel::load(JSUMemoryInputStream& stream)
-{
-	JDrama::TNameRef::load(stream);
-	// TODO: load sun model params
-}
-
-void TSunModel::perform(u32 flags, JDrama::TGraphics* gfx)
-{
-	(void)flags;
-	(void)gfx;
-	// TODO: draw sun
 }
