321f079df0d053528b4f39e666e7d6e1fd701f19
trader-vps <trader-vps@btc-predictor>
2026-05-28 02:50:45 +0000

    NpcBase: moveObject 74% + calcRootMatrix 57% + execMotionBlend_ 53% + receiveMessage 62%
    Implemented 4 medium-size functions with correct logic.
    
    - moveObject       0 -> 74.42% (1124-byte virtual override; ensureTakeSituation,
                                    trample, inbetween, coin/balloon update, forbid
                                    counts, ground actor sand-bomb, control/bind
                                    virtual calls, mHolder rotation/position-from-
                                    takingMtx branch, calcRidePos)
    - calcRootMatrix   0 -> 57.42% (mActorType==0x1D fallback + SDLModel/mHolder
                                    takingMtx PSMTXCopy chain)
    - execMotionBlend_ 0 -> 52.84% (motion-blend predicate-driven setKeepAnm + tick)
    - receiveMessage   0 -> 62.10% (message 4/0/0xF/0xE/0x10 dispatch with
                                    mActorType switches for take/trample/hit kinds
                                    and 3-particle emit on first 0xE/0x10 hit)
    
    NpcBase TU 12.95 -> 28.52% fuzzy (+15.6pp).
    Project 68.3603 -> 68.4013% (+0.041pp).

--- src/NPC/NpcBase.cpp
+++ src/NPC/NpcBase.cpp
@@ -1,12 +1,20 @@
+#include <Camera/cameralib.hpp>
+#include <System/Particles.hpp>
 #include <JSystem/JDrama/JDRActor.hpp>
 #include <JSystem/JGeometry/JGVec3.hpp>
 #include <M3DUtil/MActor.hpp>
 #include <M3DUtil/SDLModel.hpp>
+#include <MarioUtil/MapUtil.hpp>
 #include <NPC/NpcBalloon.hpp>
 #include <NPC/NpcBase.hpp>
+#include <NPC/NpcCoin.hpp>
 #include <NPC/NpcInbetween.hpp>
+#include <NPC/NpcNerve.hpp>
 #include <NPC/NpcSave.hpp>
+#include <NPC/NpcTrample.hpp>
 #include <Player/MarioAccess.hpp>
+#include <Strategic/Spine.hpp>
+#include <System/MarDirector.hpp>
 #include <dolphin/mtx.h>
 #include <math.h>
 
@@ -56,6 +64,217 @@ Vec TBaseNPC::getCursorPos() const
 	    mPosition.z);
 }
 
+void TBaseNPC::execMotionBlend_()
+{
+	TNpcInbetween* ib = (TNpcInbetween*)mUnk18C;
+	if (!ib->isMotionBlending())
+		setKeepAnm_();
+	ib->execMotionBlend(getMActor());
+	if (ib->isForcedBlendRatio())
+		mAnmRequest->mKind = -1;
+}
+
+void TBaseNPC::calcRootMatrix()
+{
+	if (mActorType == 0x0400001D) {
+		TLiveActor::calcRootMatrix();
+		return;
+	}
+	execMotionBlend_();
+	if (mHolder != nullptr && mSDLModel != nullptr) {
+		MtxPtr takingMtx = mHolder->getTakingMtx();
+		PSMTXCopy(takingMtx, *(MtxPtr*)((u8*)mSDLModel + 0x20));
+		mSDLModel->entry();
+		PSMTXCopy(*(MtxPtr*)((u8*)getModel() + 0x20), *mSDLMtx);
+	} else {
+		TLiveActor::calcRootMatrix();
+	}
+}
+
+void TBaseNPC::moveObject()
+{
+	if (mLiveFlag & 1)
+		return;
+
+	ensureTakeSituation();
+
+	if (mHolder != nullptr && mHolder->mHeldObject != this)
+		releaseTaken_();
+
+	if (mNpcTrample != nullptr) {
+		if (mNpcTrample->updateTrample(mEffectScaleBase.y, &mEffectScaleBase.y)
+		    && isNerveCanGoToMad() && isStateGoToMad_())
+			changeNerveToMad_();
+	}
+
+	{
+		TNpcInbetween* ib = (TNpcInbetween*)mUnk18C;
+		if (ib->mPosInbetweenTimer > 0)
+			mPosition = ib->mCurrentPos;
+	}
+
+	if (mNpcCoin != nullptr)
+		mNpcCoin->updateCoin();
+
+	if (mNpcBalloon != nullptr) {
+		u32 prevMsg = (u32)mNpcBalloon->_000;
+		u8  state   = gpMarDirector->unk124;
+		bool isEvent = (state == 1 || state == 2 || state == 3 || state == 4);
+		if (isEvent && mNpcBalloon->updateBalloon()) {
+			if (mHolder != nullptr) {
+				if (prevMsg < 0xE0050) {
+					if (prevMsg >= 0xE004F)
+						mNpcBalloon->setNextMessage(0xE004F, 0x1C20);
+				} else if (prevMsg > 0xE0050) {
+					if (prevMsg < 0xE0052)
+						mNpcBalloon->setNextMessage(0xE0051, 0x1C20);
+				}
+			} else {
+				if (prevMsg == 0xE0050)
+					mNpcBalloon->setNextMessage(0xE0050, 0x1C20);
+			}
+		}
+	}
+
+	if (unk1E0 != 0)
+		unk1E0 = unk1E0 - 1;
+	if (unk1E2 != 0)
+		unk1E2 = unk1E2 - 1;
+	if (unk1E4 != 0)
+		unk1E4 = unk1E4 - 1;
+
+	mAngularVelocity.x = 0.0f;
+
+	if (SMS_GetGroundActor(mGroundPlane, 0) != nullptr) {
+		mHeadHeight = mPtrSaveNormal->mSLHeadHeightSandBomb.value;
+		behaveToSandBomb_(SMS_GetGroundActor(mGroundPlane, 0));
+	} else {
+		mHeadHeight = mPtrSaveNormal->mSLHeadHeightNormal.value;
+		unk1C8      = 0.0f;
+	}
+
+	control();
+
+	if (!(mLiveFlag & 0x80))
+		calcRideMomentum();
+
+	if ((mLiveFlag & 0x80) && !(mLiveFlag & 0x200))
+		emitSinkEffect_();
+
+	const TNerveBase<TLiveActor>* latest = mSpine->getLatestNerve();
+	if (latest == &TNerveNPCSetPosAfterSinkBottom::theNerve())
+		return;
+
+	execNpcObjCollision_();
+	if (!(mLiveFlag & 0x20))
+		bind();
+
+	if (mHolder != nullptr) {
+		MtxPtr mtx     = mHolder->getTakingMtx();
+		mPosition.x    = mtx[0][3];
+		mPosition.y    = mtx[1][3];
+		mPosition.z    = mtx[2][3];
+		if (mSDLModel == nullptr) {
+			f32 angle = CLBRoundf<s16>((180.0f / 3.14159265358979f) * mtx[1][0]);
+			mRotation.y
+			    = (f32)(s16)(angle - mAngleYDiffWhenTaken) * (360.0f / 65536.0f);
+		}
+	} else {
+		if (!(mLiveFlag & 0x20) && isNerveWalk() && belongToGround() == 0
+		    && mLinearVelocity.y > 0.0f)
+			mLinearVelocity.y = 0.0f;
+		mPosition.x += mLinearVelocity.x;
+		mPosition.y += mLinearVelocity.y;
+		mPosition.z += mLinearVelocity.z;
+		mRotation.x += mAngularVelocity.x;
+		mRotation.y += mAngularVelocity.y;
+		mRotation.z += mAngularVelocity.z;
+	}
+
+	calcRidePos();
+}
+
+BOOL TBaseNPC::receiveMessage(THitActor* sender, u32 message)
+{
+	BOOL result = TRUE;
+	if (mActorType == 0x0400001C)
+		return result;
+
+	if (message == 4) {
+		if ((mLiveFlag & 0x100000) && mHolder == nullptr) {
+			behaveToBeTaken_(sender);
+			return TRUE;
+		}
+	}
+
+	if (message == 0) {
+		behaveToBeTrampled_();
+		return TRUE;
+	}
+
+	if (message == 0xF) {
+		bool shouldBehave = true;
+		switch (mActorType) {
+		case 0x04000007:
+		case 0x04000008:
+		case 0x0400000F:
+		case 0x04000014:
+		case 0x0400001C:
+		case 0x0400001D:
+			shouldBehave = false;
+		}
+		if (shouldBehave)
+			behaveToHitObject_(sender, (EnumHitNpcObjectKind)0);
+		return TRUE;
+	}
+
+	bool kinoMatch = false;
+	if (message == 0x10) {
+		kinoMatch = true;
+	} else if (message == 0xE) {
+		if (sender->mActorType != 0x4000005A)
+			return TRUE;
+		kinoMatch = true;
+	}
+	if (!kinoMatch)
+		return TRUE;
+
+	if (unk1E4 == 0) {
+		JGeometry::TVec3<f32> scale(1.0f, 1.0f, 1.0f);
+		SMS_EasyEmitParticle((E_SMS_EFFECT_ONETIME_NORMAL)0xA,
+		                     (const JGeometry::TVec3<f32>*)&sender->mPosition,
+		                     nullptr, scale);
+		JGeometry::TVec3<f32> scale2(1.0f, 1.0f, 1.0f);
+		SMS_EasyEmitParticle((E_SMS_EFFECT_ONETIME_NORMAL)0xB,
+		                     (const JGeometry::TVec3<f32>*)&sender->mPosition,
+		                     nullptr, scale2);
+		JGeometry::TVec3<f32> scale3(1.0f, 1.0f, 1.0f);
+		SMS_EasyEmitParticle((E_SMS_EFFECT_ONETIME_NORMAL)0xC,
+		                     (const JGeometry::TVec3<f32>*)&sender->mPosition,
+		                     nullptr, scale3);
+		unk1E4 = 0x10;
+	}
+
+	bool shouldHit = isMadNpc();
+	if (!shouldHit) {
+		switch (mActorType) {
+		case 0x0400000E:
+		case 0x04000010:
+		case 0x04000011:
+		case 0x04000013:
+		case 0x04000015:
+		case 0x04000016:
+		case 0x04000017:
+			shouldHit = true;
+		}
+	}
+	if (shouldHit) {
+		EnumHitNpcObjectKind kind
+		    = message == 0x10 ? (EnumHitNpcObjectKind)1 : (EnumHitNpcObjectKind)2;
+		behaveToHitObject_(sender, kind);
+	}
+	return result;
+}
 
 BOOL TBaseNPC::isInBodyTurnSearchRange() const
 {
