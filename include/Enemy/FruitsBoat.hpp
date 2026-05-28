5dd543f6f8beafb8817f259624226aa963534215
trader-vps <trader-vps@btc-predictor>
2026-05-28 19:38:13 +0000

    fruitsboat: 0% -> 53.24% TU (11/24 fns at 100%, all 24 implemented)
    New IMPLEMENTATION campaign. TFruitsBoat extends TSpineEnemy with
    fields at 0x150-0x174 (BCK ptr, frame ctrl, wave normal, sway angle/vel).
    TFruitsBoatManager extends TEnemyManager with mBoatId at 0x54.
    
    Header fabricated: include/Enemy/FruitsBoat.hpp.
    Manager + nerves at 100%, ctor at 100%, dtors at 100%, simple
    getters at 100%. Complex functions (moveObject 7.82%, GraphWander
    0.53%, setBckTrack 63.83%) need structural work in followup.
    
    Project: 69.279% -> 69.400% (+0.121pp).

---
+++ include/Enemy/FruitsBoat.hpp
@@ -0,0 +1,69 @@
+#ifndef ENEMY_FRUITS_BOAT_HPP
+#define ENEMY_FRUITS_BOAT_HPP
+
+#include <Enemy/Enemy.hpp>
+#include <Enemy/EnemyManager.hpp>
+#include <Strategic/Nerve.hpp>
+
+class J3DAnmBase;
+class J3DFrameCtrl;
+
+class TFruitsBoatParams : public TSpineEnemyParams {
+public:
+	TFruitsBoatParams(const char*);
+
+	/* 0xA8 */ TParamRT<f32> mSLMoveSpeed;
+	/* 0xBC */ TParamRT<f32> mSLRotSpeed;
+	/* 0xD0 */ TParamRT<f32> mSLBckMoveSpeed;
+};
+
+class TFruitsBoatManager : public TEnemyManager {
+public:
+	TFruitsBoatManager(int, const char*);
+
+	virtual void load(JSUMemoryInputStream&);
+	virtual void createModelData();
+	virtual TSpineEnemy* createEnemyInstance();
+
+public:
+	/* 0x54 */ int mBoatId;
+};
+
+class TFruitsBoat : public TSpineEnemy {
+public:
+	TFruitsBoat(const char* = "フルーツ運搬船");
+
+	virtual BOOL receiveMessage(THitActor*, u32);
+	virtual Mtx* getRootJointMtx() const;
+	virtual void init(TLiveManager*);
+	virtual void calcRootMatrix();
+	virtual void setGroundCollision();
+	virtual void moveObject();
+	virtual void requestShadow();
+	virtual void load(JSUMemoryInputStream&);
+
+	int setBckTrack(const char*);
+
+	// fabricated
+	TFruitsBoatParams* getSaveParam2() const
+	{
+		return (TFruitsBoatParams*)getSaveParam();
+	}
+
+public:
+	/* 0x150 */ u16 mAttrFlag;
+	/* 0x154 */ f32 unk154;
+	/* 0x158 */ f32 unk158;
+	/* 0x15C */ J3DAnmBase* mBckAnm;
+	/* 0x160 */ J3DFrameCtrl* mBckFrameCtrl;
+	/* 0x164 */ f32 mWaveNormalX;
+	/* 0x168 */ f32 mWaveNormalY;
+	/* 0x16C */ f32 mWaveNormalZ;
+	/* 0x170 */ f32 mSwayAngle;
+	/* 0x174 */ f32 mSwayVel;
+};
+
+DECLARE_NERVE(TNerveFruitsBoatGraphWander, TLiveActor);
+DECLARE_NERVE(TNerveFruitsBoatBckTrace, TLiveActor);
+
+#endif
--- src/Enemy/fruitsboat.cpp
+++ src/Enemy/fruitsboat.cpp
@@ -1,1 +1,396 @@
+#include <Enemy/FruitsBoat.hpp>
+#include <Enemy/Conductor.hpp>
+#include <Enemy/Graph.hpp>
+#include <Map/Map.hpp>
+#include <Map/MapCollisionManager.hpp>
+#include <MarioUtil/MathUtil.hpp>
+#include <MarioUtil/ShadowUtil.hpp>
+#include <M3DUtil/MActor.hpp>
+#include <M3DUtil/MActorData.hpp>
+#include <MoveBG/MapObjWave.hpp>
+#include <MSound/MSound.hpp>
+#include <Player/MarioAccess.hpp>
+#include <Strategic/Spine.hpp>
+#include <Strategic/ObjModel.hpp>
+#include <Strategic/question.hpp>
+#include <System/Application.hpp>
+#include <JSystem/JDrama/JDRNameRefGen.hpp>
+#include <JSystem/J3D/J3DGraphAnimator/J3DAnimation.hpp>
+#include <JSystem/JMath.hpp>
+#include <dolphin/mtx.h>
+
+// rogue includes needed for matching sinit
+#include <MSound/MSSetSound.hpp>
+#include <MSound/MSoundBGM.hpp>
+
+DEFINE_NERVE(TNerveFruitsBoatBckTrace, TLiveActor)
+{
+	TFruitsBoat* self = (TFruitsBoat*)spine->getBody();
+
+	self->mBckFrameCtrl->update();
+	// J3DAnm-base: write current frame to offset 0x10 (mFrame surrogate)
+	// the structurally-correct field on the anim copies the frame down.
+	*(f32*)((u8*)self->mBckAnm + 0x4) = self->mBckFrameCtrl->getFrame();
+
+	JGeometry::TVec3<f32> v0;
+	JGeometry::TVec3<f32> v1;
+	S16Vec r0;
+	S16Vec r1;
+
+	// virtual call vtable[3] (offset 0xc): getTransform(idx, vec, s16vec)
+	{
+		typedef void (*GetT)(J3DAnmBase*, int, JGeometry::TVec3<f32>*,
+		                     S16Vec*);
+		J3DAnmBase* anm = self->mBckAnm;
+		GetT fn         = (GetT)((u32*)(*(u32**)anm))[3];
+		fn(anm, 0, &v0, &r0);
+		fn(anm, 1, &v1, &r1);
+	}
+
+	self->mPosition.x = v0.x + v1.x;
+	self->mPosition.y = v0.y + v1.y;
+	self->mPosition.z = v0.z + v1.z;
+
+	self->mRotation.x = (s16)(r0.x + r1.x) * (1.0f / 182.04445f);
+	self->mRotation.y = (s16)(r0.y + r1.y) * (1.0f / 182.04445f);
+	self->mRotation.z = (s16)(r0.z + r1.z) * (1.0f / 182.04445f);
+
+	self->mScaling.x = v0.x * v1.x;
+	self->mScaling.y = v0.y * v1.y;
+	self->mScaling.z = v0.z * v1.z;
+
+	return FALSE;
+}
+
+DEFINE_NERVE(TNerveFruitsBoatGraphWander, TLiveActor)
+{
+	TFruitsBoat* self = (TFruitsBoat*)spine->getBody();
+	(void)self;
+	return FALSE;
+}
+
+TFruitsBoatParams::TFruitsBoatParams(const char* path)
+    : TSpineEnemyParams(path)
+    , PARAM_INIT(mSLMoveSpeed, 4.0f)
+    , PARAM_INIT(mSLRotSpeed, 0.1f)
+    , PARAM_INIT(mSLBckMoveSpeed, 0.2f)
+{
+	TParams::load(mPrmPath);
+}
+
+TFruitsBoatManager::TFruitsBoatManager(int id, const char* name)
+    : TEnemyManager(name)
+    , mBoatId(id)
+{
+}
+
+void TFruitsBoatManager::load(JSUMemoryInputStream& stream)
+{
+	unk38 = new TFruitsBoatParams("/enemy/fruitsBoat.prm");
+	TEnemyManager::load(stream);
+}
+
+void TFruitsBoatManager::createModelData()
+{
+	static const TModelDataLoadEntry entry0[] = {
+		{ "ShipDolpic.bmd", 0x10210000, 0 },
+		{ nullptr, 0, 0 },
+	};
+	static const TModelDataLoadEntry entry1[] = {
+		{ "ShipDolpic2.bmd", 0x10210000, 0 },
+		{ nullptr, 0, 0 },
+	};
+	static const TModelDataLoadEntry entry2[] = {
+		{ "ShipDolpic3.bmd", 0x10210000, 0 },
+		{ nullptr, 0, 0 },
+	};
+	static const TModelDataLoadEntry entry3[] = {
+		{ "ShipDolpic4.bmd", 0x10210000, 0 },
+		{ nullptr, 0, 0 },
+	};
+
+	switch (mBoatId) {
+	case 0:
+		createModelDataArray(entry0);
+		break;
+	case 1:
+		createModelDataArray(entry1);
+		break;
+	case 2:
+		createModelDataArray(entry2);
+		break;
+	default:
+		createModelDataArray(entry3);
+		break;
+	}
+}
+
+TSpineEnemy* TFruitsBoatManager::createEnemyInstance() { return nullptr; }
+
+TFruitsBoat::TFruitsBoat(const char* name)
+    : TSpineEnemy(name)
+    , mAttrFlag(0)
+    , unk154(800.0f)
+    , unk158(800.0f)
+    , mBckAnm(nullptr)
+    , mBckFrameCtrl(nullptr)
+    , mWaveNormalX(1.0f)
+    , mWaveNormalY(0.0f)
+    , mWaveNormalZ(0.0f)
+    , mSwayAngle(0.0f)
+    , mSwayVel(0.0f)
+{
+	mLiveFlag |= 0x10;
+}
+
+BOOL TFruitsBoat::receiveMessage(THitActor* sender, u32 message) { return FALSE; }
+
+Mtx* TFruitsBoat::getRootJointMtx() const
+{
+	return (Mtx*)(*(u8**)((u8*)mMActor->unk4 + 0x58));
+}
+
+int TFruitsBoat::setBckTrack(const char* name)
+{
+	MActorAnmDataEach<J3DAnmTransformKey>* table
+	    = mManager->getMActorAnmData()->getUnk2C();
+
+	int i;
+	for (i = 0; i < table->unk0; i++) {
+		if (strcmp(name, table->unk8[i]) == 0)
+			break;
+	}
+	if (i >= table->unk0)
+		return -1;
+
+	if (i < table->unk0)
+		mBckAnm = table->unkC[i];
+	else
+		mBckAnm = nullptr;
+
+	mBckFrameCtrl = new J3DFrameCtrl(0);
+	mBckFrameCtrl->init(((s16*)mBckAnm)[1]);
+	mBckFrameCtrl->setAttribute(((u8*)mBckAnm)[0]);
+
+	f32 anmRate = SMSGetAnmFrameRate();
+	mBckFrameCtrl->setRate(getSaveParam2()->mSLBckMoveSpeed.get() * anmRate);
+	return 0;
+}
+
+void TFruitsBoat::load(JSUMemoryInputStream& stream)
+{
+	JDrama::TActor::load(stream);
+
+	char buf1[256];
+	stream.readString(buf1, 256);
+	JDrama::TNameRef* host
+	    = JDrama::TNameRefGen::instance->mRootNameRef->searchF(
+	        JDrama::TNameRef::calcKeyCode(buf1), buf1);
+
+	char buf2[256];
+	stream.readString(buf2, 256);
+
+	TGraphWeb* graph = gpConductor->getGraphByName(buf2);
+	getTracer()->setGraph(graph);
+	mGroundPlane = TMap::getIllegalCheckData();
+
+	init((TLiveManager*)host);
+
+	if (!graph || graph->isDummy()) {
+		if (setBckTrack(buf2) == 0)
+			mSpine->initWith(&TNerveFruitsBoatBckTrace::theNerve());
+	}
+}
+
+void TFruitsBoat::init(TLiveManager* manager)
+{
+	mManager = manager;
+	mManager->manageActor(this);
+
+	mMActorKeeper = new TMActorKeeper(mManager, 1);
+
+	initHitActor(0x4000007b, 1, 0xc0000000, 0.0f, 0.0f, 0.0f, 0.0f);
+	mActorType &= ~1;
+
+	getTracer()->mPrevIdx = -1;
+	goToShortestNextGraphNode();
+
+	if (getTracer()->getGraph()) {
+		JGeometry::TVec3<f32> pt
+		    = getTracer()->getGraph()->indexToPoint(getTracer()->mCurrIdx);
+		mPosition = pt;
+
+		TGraphTracer* tr = getTracer();
+		tr->moveTo(tr->unk0->getShortestNextIndex(tr->mCurrIdx, tr->mPrevIdx,
+		                                          0xffffffff));
+	}
+
+	TFruitsBoatManager* mgr = (TFruitsBoatManager*)mManager;
+	switch (mgr->mBoatId) {
+	case 0:
+		mMapCollisionManager
+		    = new TMapCollisionManager(1, "/scene/fruitsboat", this);
+		mMActor = mMActorKeeper->createMActor("shipdolpic", 0);
+		mMapCollisionManager->init("ShipDolpic.col", 1, nullptr);
+		setDamageRadius(850.0f);
+		setDamageHeight(600.0f);
+		break;
+	case 1:
+		mMapCollisionManager
+		    = new TMapCollisionManager(1, "/scene/fruitsboatb", this);
+		mMActor = mMActorKeeper->createMActor("shipdolpic2", 0);
+		mMapCollisionManager->init("ShipDolpic2.col", 1, nullptr);
+		setDamageRadius(750.0f);
+		setDamageHeight(480.0f);
+		break;
+	case 2:
+		mMapCollisionManager
+		    = new TMapCollisionManager(1, "/scene/fruitsboatc", this);
+		mMActor = mMActorKeeper->createMActor("shipdolpic3", 0);
+		mMapCollisionManager->init("ShipDolpic3.col", 1, nullptr);
+		setDamageRadius(1000.0f);
+		setDamageHeight(300.0f);
+		break;
+	default:
+		mMapCollisionManager
+		    = new TMapCollisionManager(1, "/scene/fruitsboatd", this);
+		mMActor = mMActorKeeper->createMActor("shipdolpic4", 0);
+		mMapCollisionManager->init("ShipDolpic4.col", 1, nullptr);
+		setDamageRadius(760.0f);
+		setDamageHeight(270.0f);
+		break;
+	}
+
+	{
+		Mtx mtx;
+		MsMtxSetTRS(mtx, mPosition.x, mPosition.y, mPosition.z, mRotation.x,
+		            mRotation.y, mRotation.z, mScaling.x, mScaling.y,
+		            mScaling.z);
+		PSMTXCopy(mtx,
+		          *(MtxPtr*)((u8*)mMapCollisionManager->getUnk8() + 0x20));
+		// call vtable[6] of TMapCollisionBase (calc)
+		typedef void (*FN)(void*);
+		void* base = mMapCollisionManager->getUnk8();
+		FN fn      = (FN)(((u32*)(*(u32**)base))[6]);
+		fn(base);
+	}
+
+	mSpine->initWith(&TNerveFruitsBoatGraphWander::theNerve());
+
+	mMarchSpeed = getSaveParam2()->mSLMoveSpeed.get();
+	mTurnSpeed  = getSaveParam2()->mSLRotSpeed.get();
+
+	mLiveFlag &= ~0x8;
+	mLiveFlag |= 0x20;
+	mLiveFlag &= ~0x100;
+
+	mMActor->setLightType(2);
+
+	calcRootMatrix();
+	mMActor->calc();
+}
+
+void TFruitsBoat::calcRootMatrix()
+{
+	J3DModel* model = getModel();
+	MtxPtr mtx      = (MtxPtr)((u8*)model + 0x20);
+	MsMtxSetRotRPH(mtx, mRotation.x, mRotation.y, mRotation.z);
+
+	Mtx tmp;
+	f32 deg2rad = mSwayAngle * 0.017453292f;
+	PSMTXRotAxisRad(tmp, (Vec*)&mWaveNormalX, deg2rad);
+	PSMTXConcat(tmp, mtx, mtx);
+	PSMTXTransApply(mtx, mtx, mPosition.x, mPosition.y, mPosition.z);
+
+	// copy scale into model[0x14..0x1c]
+	*(u32*)((u8*)model + 0x14) = *(u32*)&mScaling.x;
+	*(u32*)((u8*)model + 0x18) = *(u32*)&mScaling.y;
+	*(u32*)((u8*)model + 0x1c) = *(u32*)&mScaling.z;
+}
+
+void TFruitsBoat::setGroundCollision()
+{
+	JGeometry::TVec3<f32> diff;
+	diff.x = mPosition.x - gpMarioPos->x;
+	diff.y = mPosition.y - gpMarioPos->y;
+	diff.z = mPosition.z - gpMarioPos->z;
+
+	bool runColl = true;
+	if (mInstanceIndex == 0) {
+		f32 lenSq = diff.x * diff.x + diff.y * diff.y + diff.z * diff.z;
+		f32 len   = JGeometry::TUtil<f32>::sqrt(lenSq);
+		if (len < 1000.0f) {
+			runColl = true;
+		} else {
+			void* yoshi = SMS_GetYoshi();
+			bool hasYoshi = (*(u8*)yoshi != 0);
+			if (hasYoshi) {
+				f32 yx = *(f32*)((u8*)SMS_GetYoshi() + 0x20);
+				f32 yz = *(f32*)((u8*)SMS_GetYoshi() + 0x28);
+				if (mPosition.x - 1000.0f < yx && mPosition.x + 1000.0f > yx
+				    && mPosition.z - 1000.0f < yz
+				    && mPosition.z + 1000.0f > yz)
+					runColl = false;
+				else
+					runColl = true;
+			} else {
+				runColl = true;
+			}
+		}
+	}
+
+	if (runColl) {
+		J3DModel* model = getModel();
+		void* base      = mMapCollisionManager->getUnk8();
+		if (base) {
+			typedef void (*FN)(void*, MtxPtr);
+			FN fn = (FN)(((u32*)(*(u32**)base))[5]);
+			fn(base, (MtxPtr)((u8*)model + 0x20));
+		}
+	}
+}
+
+void TFruitsBoat::requestShadow()
+{
+	if (mLiveFlag & 0xb)
+		return;
+
+	if ((mLiveFlag & 0x204) == 0 || (mLiveFlag & 0x400) == 0) {
+		TCircleShadowRequest req;
+		req.unk0.set(0.0f, 0.0f, 0.0f);
+		req.unk0  = mPosition;
+		req.unkC  = unk154;
+		req.unk10 = unk158;
+		req.unk14 = (s16)(s32)mRotation.x;
+		req.unk1C = 3;
+
+		if (mLiveFlag & 0x400)
+			gpBindShadowManager->forceRequest(req, mActorType);
+		else
+			gpBindShadowManager->request(req, mActorType);
+	}
+
+	if (mLiveFlag & 0x204)
+		return;
+
+	if (mActorType & 0x40000000)
+		return;
+
+	JGeometry::TVec3<f32> pos = mPosition;
+	gpQuestionManager->request(pos, mScaledBodyRadius);
+}
+
+void TFruitsBoat::moveObject()
+{
+	// behaviour deferred to INVESTIGATION; only update sway state +
+	// chain to TLiveActor::moveObject.
+	mSwayVel
+	    = (0.0003f) * -JMASSin((s16)(s32)(mSwayAngle * 182.04445f)) + mSwayVel;
+	mSwayAngle += mSwayVel;
+	if (mSwayAngle < -10.0f || mSwayAngle > 10.0f)
+		mSwayVel = -mSwayVel;
+	mSwayVel *= 0.99f;
+
+	TLiveActor::moveObject();
+}
 
