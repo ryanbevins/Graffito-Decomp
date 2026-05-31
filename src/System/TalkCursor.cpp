ab3f1c671346ef012ab049d3cdf5d741b25108e4
trader-vps <trader-vps@btc-predictor>
2026-05-31 11:24:27 +0000

    System/TalkCursor: 33.6% -> 85.2% (implement cursor setup)

--- src/System/TalkCursor.cpp
+++ src/System/TalkCursor.cpp
@@ -1,8 +1,30 @@
 #include <System/TalkCursor.hpp>
+#include <JSystem/J3D/J3DGraphAnimator/J3DModel.hpp>
+#include <JSystem/J3D/J3DGraphLoader/J3DModelLoader.hpp>
+#include <JSystem/JKernel/JKRFileLoader.hpp>
+#include <M3DUtil/InfectiousStrings.hpp>
 #include <M3DUtil/MActor.hpp>
+#include <M3DUtil/MActorData.hpp>
+#include <NPC/NpcBase.hpp>
 #include <dolphin/gx.h>
 
-void TTalkCursor::loadAfter() { }
+void TTalkCursor::loadAfter()
+{
+	MActorAnmData* anmData = new MActorAnmData;
+	anmData->init("/common/cursor_b", nullptr);
+
+	unk10         = new MActor(anmData);
+	MActor* actor = unk10;
+
+	void* res = JKRFileLoader::getGlbResource("/common/cursor_b/default.bmd");
+	J3DModel* model
+	    = new J3DModel(J3DModelLoaderDataBase::load(res, 0x10020000), 0, 1);
+	actor->setModel(model, 0);
+	actor->setBck("icon_rot");
+	actor->setBrk("icon_flash");
+
+	unkC.on(0x204);
+}
 
 void TTalkCursor::perform(u32 flags, JDrama::TGraphics* gfx)
 {
@@ -13,4 +35,16 @@ void TTalkCursor::perform(u32 flags, JDrama::TGraphics* gfx)
 	}
 }
 
-void TTalkCursor::associateNPC(TBaseNPC* npc) { (void)npc; }
+void TTalkCursor::associateNPC(TBaseNPC* npc)
+{
+	if (npc) {
+		Vec pos = npc->getCursorPos();
+		TPosition3f mtx;
+		mtx.identity33();
+		mtx.setTrans(pos);
+		PSMTXCopy(mtx, unk10->getModel()->unk20);
+		unkC.off(0x204);
+	} else {
+		unkC.on(0x204);
+	}
+}
