37a8adee1fba3543e992da0226b0fe157326fd8f
trader-vps <trader-vps@btc-predictor>
2026-05-31 16:15:05 +0000

    Map/PollutionObj: 64.2% -> 99.8% (recover depth helpers)

--- src/Map/PollutionObj.cpp
+++ src/Map/PollutionObj.cpp
@@ -1,5 +1,7 @@
 #include <Map/PollutionObj.hpp>
 #include <Map/PollutionLayer.hpp>
+#include <Map/Map.hpp>
+#include <Map/MapEventSink.hpp>
 #include <JSystem/J3D/J3DGraphAnimator/J3DJoint.hpp>
 #include <types.h>
 
@@ -7,20 +9,71 @@
 #include <MSound/MSSetSound.hpp>
 #include <MSound/MSoundBGM.hpp>
 
-void is_near(float, float) { }
+static inline bool is_near(f32 a, f32 b)
+{
+	if (a - 30.0f <= b && b <= a + 30.0f)
+		return true;
 
-void is_near(float, float, float, float) { }
+	return false;
+}
+
+static inline bool is_near(f32 a, f32 b, f32 c, f32 d)
+{
+	if (is_near(a, b) && is_near(b, c) && is_near(c, d))
+		return true;
+
+	return false;
+}
 
 int TPollutionLayer::getTexPosT(f32 z) const
 {
 	return unk5C.worldToTexSize(z - unk40);
 }
 
-void TPollutionObj::getDepthFromMap(int, int) { }
+u8 TPollutionObj::getDepthFromMap(int x, int y)
+{
+	const TBGCheckData* checkData;
+	f32 scale = unk34->unk5C.mVerticalScale;
+	f32 scaledX = x * scale;
+	f32 scaledZ = y * scale;
+	f32 worldX  = unk34->unk38;
+	f32 worldZ  = unk34->unk40;
+	worldX += scaledX;
+	worldZ += scaledZ;
 
-void TPollutionObj::updateDepthMap() { }
+	f32 minX = worldX - 5.0f;
+	f32 maxX = worldX + scale + 5.0f;
+	f32 minZ = worldZ - 5.0f;
+	f32 maxZ = worldZ + scale + 5.0f;
 
-bool TPollutionObj::isCleaned() const { }
+	f32 ground0 = gpMap->checkGround(minX, 9999999.0f, minZ, &checkData);
+	f32 ground1 = gpMap->checkGround(maxX, 9999999.0f, minZ, &checkData);
+	f32 ground2 = gpMap->checkGround(minX, 9999999.0f, maxZ, &checkData);
+	f32 ground3 = gpMap->checkGround(maxX, 9999999.0f, maxZ, &checkData);
+
+	if (is_near(ground0, ground2, ground1, ground3)) {
+		f32 halfScale = 0.5f * scale;
+		f32 ground = gpMap->checkGround(worldX + halfScale, 9999999.0f,
+		                                worldZ + halfScale, &checkData);
+		return unk34->unk5C.worldToDepth(ground);
+	}
+
+	return 0xff;
+}
+
+void TPollutionObj::updateDepthMap()
+{
+	for (int y = unk28; y < unk2C; ++y)
+		for (int x = unk20; x < unk24; ++x)
+			unk34->unk5C.setDepth(x, y, getDepthFromMap(x, y));
+}
+
+bool TPollutionObj::isCleaned() const
+{
+	if (unk30 < TMapEventSink::mCleanedDegree)
+		return true;
+	return false;
+}
 
 void TPollutionObj::initAreaInfo(TPollutionLayer* layer)
 {
