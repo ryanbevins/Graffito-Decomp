58f7fb3f0ba358fde3771e37ee6efd77d354089a
trader-vps <trader-vps@btc-predictor>
2026-06-02 13:42:15 +0000

    MSoundMainSide: 10.6% -> 19.8% (restore stage basics)

--- include/System/MSoundMainSide.hpp
+++ include/System/MSoundMainSide.hpp
@@ -7,7 +7,10 @@ class MSStage {
 public:
 	virtual void setPosPtr(Vec*);
 	virtual void stageLoop();
+	virtual void proc() = 0;
 	static MSStage* init(u8, u8);
+
+	static MSStage* smMSStage;
 };
 
 class MSSTageSimpleEnvironmentMonte {
@@ -15,41 +18,88 @@ public:
 	void proc();
 };
 
-class MSSTageSimpleEnvironment {
+class MSSTageSimpleEnvironment : public MSStage {
 public:
-	void proc();
+	MSSTageSimpleEnvironment(u32 sound_id)
+	    : mSoundID(sound_id)
+	{
+	}
+
+	virtual void proc();
+
+	/* 0x04 */ u32 mSoundID;
 };
 
-class MSStageCubeSwitch {
+class MSStageCubeFade : public MSStage {
 public:
+	MSStageCubeFade();
+
+	void calcParamRatioInCube(long);
+	virtual void proc();
+
+	/* 0x04 */ s32 mCurrentCube;
+	/* 0x08 */ s32 mPreviousCube;
+	/* 0x0C */ f32 mFadeRatio;
+};
+
+class MSStageCubeSwitch : public MSStageCubeFade {
+public:
+	MSStageCubeSwitch(bool flag)
+	    : MSStageCubeFade()
+	    , unk10(flag)
+	{
+	}
+
 	void toBossBgm();
 	void toStageBgm();
-	void proc();
+	virtual void proc();
+
+	/* 0x10 */ bool unk10;
+	/* 0x11 */ bool unk11;
 };
 
-class MSStageCubeFade {
+class MSStageCubeFadeMonte : public MSStageCubeFade {
 public:
-	void calcParamRatioInCube(long);
-	void proc();
-	MSStageCubeFade();
+	MSStageCubeFadeMonte()
+	    : MSStageCubeFade()
+	    , unk10(3)
+	    , unk14(3)
+	{
+	}
+
+	virtual void proc();
+
+	/* 0x10 */ s32 unk10;
+	/* 0x14 */ s32 unk14;
 };
 
-class MSStageCubeFadeMonte {
+class MSStageDistFade : public MSStage {
 public:
-	void proc();
-	MSStageCubeFadeMonte();
-};
-
-class MSStageDistFadeMonte {
-public:
-	void proc();
-	MSStageDistFadeMonte(const Vec*, float, float, u32, bool);
-};
-
-class MSStageDistFade {
-public:
-	void proc();
 	MSStageDistFade(const Vec*, float, float, u32, bool);
+	virtual void proc();
+
+	/* 0x04 */ s32 unk4;
+	/* 0x08 */ f32 unk8;
+	/* 0x0C */ f32 unkC;
+	/* 0x10 */ const Vec* unk10;
+	/* 0x14 */ u32 unk14;
+	/* 0x18 */ bool unk18;
+};
+
+class MSStageDistFadeMonte : public MSStageDistFade {
+public:
+	MSStageDistFadeMonte(const Vec* pos, float near_dist, float far_dist,
+	                     u32 bgm, bool use_pan)
+	    : MSStageDistFade(pos, near_dist, far_dist, bgm, use_pan)
+	    , unk1C(3)
+	    , unk20(3)
+	{
+	}
+
+	virtual void proc();
+
+	/* 0x1C */ s32 unk1C;
+	/* 0x20 */ s32 unk20;
 };
 
 class MSStageProc {
@@ -61,9 +111,22 @@ class MSMainProc {
 public:
 	class MSStageInfo {
 	public:
-		static bool bossNotDamaged;
+		static u32 msStg;
+		static u32 demoBgm;
+		static u32 stageBgm;
+		static u32 stageBgmSilent;
+		static u8 stageBgmSilentStartStatus;
+		static u8 flags;
+		static u16 volOffCategory;
+		static u8 fadeEvent;
+		static u32 switchBgm;
+		static u32 switchBgm2;
+		static f32 cubeFadeRatio;
+		static bool cubeFadeUsePan;
 		static bool bossLives;
 		static bool bossLives2;
+		static bool bossNotDamaged;
+		static bool distFadeStageToKage;
 	};
 
 	static void startStageBGM(u8, u8);
@@ -84,7 +147,14 @@ public:
 	static void fromInnerCameraDemo();
 	static void toInnerCameraDemo();
 	void entranceDemoWipeInEnd();
-	void getMonteVillageActorArea(const Vec&);
+	static u32 getMonteVillageActorArea(const Vec&);
 };
+
+inline MSStageCubeFade::MSStageCubeFade()
+    : mCurrentCube(-1)
+    , mPreviousCube(-1)
+    , mFadeRatio(MSMainProc::MSStageInfo::cubeFadeRatio)
+{
+}
 
 #endif
--- src/System/MSoundMainSide.cpp
+++ src/System/MSoundMainSide.cpp
@@ -1,12 +1,51 @@
 #include <System/MSoundMainSide.hpp>
+#include <Camera/CubeManagerBase.hpp>
 #include <MSound/MSound.hpp>
 #include <MSound/MSoundBGM.hpp>
+#include <MSound/MSoundSE.hpp>
+#include <Player/MarioAccess.hpp>
+#include <System/MarDirector.hpp>
 
-bool MSMainProc::MSStageInfo::bossNotDamaged;
+static const u32 cMSBgmNone = 0xfffffff0;
+
+u32 MSMainProc::MSStageInfo::msStg;
+u32 MSMainProc::MSStageInfo::demoBgm;
+u32 MSMainProc::MSStageInfo::stageBgm;
+u32 MSMainProc::MSStageInfo::stageBgmSilent;
+u8 MSMainProc::MSStageInfo::stageBgmSilentStartStatus;
+u8 MSMainProc::MSStageInfo::flags;
+u16 MSMainProc::MSStageInfo::volOffCategory;
+u8 MSMainProc::MSStageInfo::fadeEvent;
+u32 MSMainProc::MSStageInfo::switchBgm;
+u32 MSMainProc::MSStageInfo::switchBgm2;
+f32 MSMainProc::MSStageInfo::cubeFadeRatio;
+bool MSMainProc::MSStageInfo::cubeFadeUsePan;
 bool MSMainProc::MSStageInfo::bossLives;
 bool MSMainProc::MSStageInfo::bossLives2;
+bool MSMainProc::MSStageInfo::bossNotDamaged;
+bool MSMainProc::MSStageInfo::distFadeStageToKage;
+MSStage* MSStage::smMSStage;
 
 void MSStage::setPosPtr(Vec*) { }
+
+void MSSTageSimpleEnvironment::proc()
+{
+	if (mSoundID != 0xffffffff)
+		MSoundSESystem::MSoundSE::startSoundSystemSE(mSoundID, 0, nullptr, 0);
+}
+
+MSStageDistFade::MSStageDistFade(const Vec* pos, float near_dist,
+                                 float far_dist, u32 bgm, bool use_pan)
+    : unk4(0)
+    , unk8(near_dist == 0.0f ? 2000.0f : near_dist)
+    , unkC(far_dist == 0.0f ? 500.0f : far_dist)
+    , unk10(pos)
+    , unk14(bgm)
+    , unk18(use_pan)
+{
+}
+
+void MSStage::stageLoop() { proc(); }
 
 void MSMainProc::setBossNotDamagedFlag(bool flag)
 {
@@ -32,7 +71,114 @@ void MSMainProc::setBossLivesFlag(bool flag)
 	MSStageInfo::bossLives = flag;
 }
 
+void MSMainProc::startStageBGM(u8, u8)
+{
+	gpMSound->initSound();
+	gpMSound->unkA0 = 0;
+	gpMSound->unkC9 = 0;
+
+	if (MSStageInfo::stageBgm != cMSBgmNone) {
+		bool shouldStart = false;
+		if (MSStageInfo::flags & 2)
+			shouldStart = true;
+
+		if (shouldStart) {
+			if (MSStageInfo::flags & 1) {
+				JAISound* sound = MSBgm::startBGM(MSStageInfo::stageBgm);
+				if (sound != nullptr)
+					sound->setVolume(0.0f, 0, 0);
+			} else {
+				MSBgm::startBGM(MSStageInfo::stageBgm);
+			}
+		}
+	}
+
+	if (MSStageInfo::stageBgmSilent != cMSBgmNone
+	    && MSStageInfo::stageBgmSilentStartStatus == 2
+	    && gpMSound->unkCF != 0) {
+		JAISound* sound = MSBgm::startBGM(MSStageInfo::stageBgmSilent);
+		if (sound != nullptr)
+			sound->setVolume(0.0f, 0, 0);
+	}
+}
+
+void MSMainProc::endStageEntranceDemo(u8, u8)
+{
+	if (MSStageInfo::demoBgm != cMSBgmNone) {
+		bool stopDemo = false;
+		if (MSStageInfo::flags & 8)
+			stopDemo = true;
+
+		if (stopDemo)
+			MSBgm::stopBGM(MSStageInfo::demoBgm, 20);
+	}
+
+	bool silentDemo = false;
+	if (MSStageInfo::flags & 4)
+		silentDemo = true;
+
+	if (silentDemo)
+		MSBgm::setVolume(MSStageInfo::demoBgm, 0.0f, 20, 0);
+
+	gpMSound->demoModeOut(false);
+	gpMSound->unkCC = 0;
+}
+
+void MSMainProc::entranceDemoLoop(u32) { }
+
+void MSMainProc::startStageEntranceDemo(u8, u8)
+{
+	if (MSStageInfo::demoBgm != cMSBgmNone)
+		MSBgm::startBGM(MSStageInfo::demoBgm);
+
+	if (MSStageInfo::stageBgmSilent != cMSBgmNone
+	    && MSStageInfo::stageBgmSilentStartStatus == 1
+	    && gpMSound->unkCF != 0) {
+		JAISound* sound = MSBgm::startBGM(MSStageInfo::stageBgmSilent);
+		if (sound != nullptr)
+			sound->setVolume(0.0f, 0, 0);
+	}
+
+	gpMSound->demoModeIn(MSStageInfo::volOffCategory, false);
+	gpMSound->unkA4 = 0;
+	gpMSound->unkC9 = 1;
+}
+
 void MSMainProc::fromTalkingCameraDemo(bool) { gpMSound->talkModeIn(false); }
+
+void MSMainProc::toTalkingCameraDemo()
+{
+	u16 category = 0;
+	switch (gpMarDirector->mMap) {
+	case 1:
+	case 4:
+	case 9:
+		category = 4;
+		break;
+	}
+	gpMSound->setCategoryVOLs(category, 1.0f);
+}
 
 void MSMainProc::fromInnerCameraDemo() { gpMSound->unkCA = 0; }
 void MSMainProc::toInnerCameraDemo() { gpMSound->unkCA = 1; }
+
+u32 MSMainProc::getMonteVillageActorArea(const Vec& pos)
+{
+	u32 area = 4;
+	if (MSGMSound->unkCD == 8) {
+		Vec checkPos = pos;
+		checkPos.y += 75.0f;
+		switch (gpCubeFastC->getInCubeNo(checkPos)) {
+		case 0:
+			area = 2;
+			break;
+		case 1:
+			area = 0;
+			break;
+		default:
+			area = 3;
+			break;
+		}
+	}
+	return area;
+}
