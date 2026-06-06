4f21d7be5b7c20f832a86f286e00508d5a3b648f
trader-vps <trader-vps@btc-predictor>
2026-06-06 09:48:34 +0000

    Camera/cameragc small helpers: 2.97% -> 3.41% (fill accessors and approach flags)

--- src/Camera/cameragc.cpp
+++ src/Camera/cameragc.cpp
@@ -5,11 +5,16 @@
 //   __ct__, ~CPolarSubCamera, loadAfter
 
 #include <Camera/Camera.hpp>
+#include <Camera/CameraInbetween.hpp>
+#include <Camera/CameraKindParam.hpp>
 #include <Camera/CameraShake.hpp>
+#include <Camera/cameralib.hpp>
 #include <Player/MarioMain.hpp>
 #include <Player/MarioAccess.hpp>
 #include <M3DUtil/MActor.hpp>
 #include <JSystem/J3D/J3DGraphAnimator/J3DModel.hpp>
+
+template <> f32 CLBLinearInbetween<f32>(f32, f32, f32);
 
 CPolarSubCamera::CPolarSubCamera(const char* name)
     : JDrama::TLookAtCamera()
@@ -30,22 +35,39 @@ s16 CPolarSubCamera::getFinalAngleZ() const
 {
 	return unk254 + gpCameraShake->mYaw;
 }
-s16 CPolarSubCamera::getOffsetAngleY() const { return 0; }
-s16 CPolarSubCamera::getOffsetAngleX() const { return 0; }
+s16 CPolarSubCamera::getOffsetAngleY() const { return unk68->unk5A; }
+s16 CPolarSubCamera::getOffsetAngleX() const { return unk68->unk58; }
 
 void CPolarSubCamera::ctrlGameCamera_() { }
 void CPolarSubCamera::calcFinalPosAndAt_() { }
 void CPolarSubCamera::calcPosAndAt_() { }
 void CPolarSubCamera::calcSlopeAngleX_(s16* out) { (void)out; }
 
-bool CPolarSubCamera::isMomentDefinite_() const { return false; }
-void CPolarSubCamera::execInvalidAutoChase_() { }
+bool CPolarSubCamera::isMomentDefinite_() const
+{
+	bool result = false;
+	if (!(unk64 & 0x100) && isNormalCameraCompletely() && unk250 > 0.0f
+	    && CLBLinearInbetween(unk68->unk10, unk68->unk14, unkA8) > 0.0f)
+		result = true;
+	return result;
+}
+
+void CPolarSubCamera::execInvalidAutoChase_() { unk284 = unk68->unk68; }
 bool CPolarSubCamera::isMarioCrabWalk_() const { return false; }
 bool CPolarSubCamera::isMarioAimWithGun_() const { return false; }
 bool CPolarSubCamera::isMarioReadyGun_() const { return false; }
 
-void CPolarSubCamera::onMoveApproach_() { }
-void CPolarSubCamera::offMoveApproach_() { }
+void CPolarSubCamera::onMoveApproach_()
+{
+	f32 dx   = mPosition.x - mTarget.x;
+	f32 dy   = mPosition.y - mTarget.y;
+	f32 dz   = mPosition.z - mTarget.z;
+	f32 dist = JGeometry::TUtil<f32>::sqrt(dx * dx + dy * dy + dz * dz);
+	unk6C->mChaseFrame
+	    = dist - CLBLinearInbetween(unk68->unk08, unk68->unk0C, unkA8);
+}
+
+void CPolarSubCamera::offMoveApproach_() { unk6C->mChaseFrame = 0.0f; }
 
 void CPolarSubCamera::rotateY_ByStickX_(f32 stick) { (void)stick; }
 void CPolarSubCamera::rotateX_ByStickY_(f32 stick) { (void)stick; }
@@ -69,7 +91,12 @@ MtxPtr CPolarSubCamera::getToroccoMtx_() const
 {
 	return gpMarioOriginal->mTorocco->unk4->mNodeMatrices[2];
 }
-bool CPolarSubCamera::isNowInbetween() const { return false; }
+bool CPolarSubCamera::isNowInbetween() const
+{
+	if (unk6C->mFrameCount > 0)
+		return true;
+	return false;
+}
 
 void CPolarSubCamera::loadAfter() { }
 
