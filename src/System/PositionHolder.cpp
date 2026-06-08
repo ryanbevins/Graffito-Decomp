03dd3d667ea458bebf76bd1eef8e1e163bb2fea4
trader-vps <trader-vps@btc-predictor>
2026-06-08 06:11:57 +0000

    System/PositionHolder load: 99.7% -> 99.7% (name discarded reads)

--- src/System/PositionHolder.cpp
+++ src/System/PositionHolder.cpp
@@ -9,11 +9,16 @@ void TStagePositionInfo::load(JSUMemoryInputStream& stream)
 	stream.read(&unkC.y, sizeof(f32));
 	stream.read(&unkC.z, sizeof(f32));
 
-	// wtf is happening with inlines here...
-	stream.readF32();
-	stream.readF32();
-	stream.readF32();
-	stream.readF32();
-	stream.readF32();
-	stream.readF32();
+	f32 unused0;
+	f32 unused1;
+	f32 unused2;
+	f32 unused3;
+	f32 unused4;
+	f32 unused5;
+	stream.read(&unused0, sizeof(f32));
+	stream.read(&unused1, sizeof(f32));
+	stream.read(&unused2, sizeof(f32));
+	stream.read(&unused3, sizeof(f32));
+	stream.read(&unused4, sizeof(f32));
+	stream.read(&unused5, sizeof(f32));
 }
