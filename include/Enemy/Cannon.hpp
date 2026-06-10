0b7406e9479464fbeffd1024d480d1485201f36e
trader-vps <trader-vps@btc-predictor>
2026-06-10 07:04:55 +0000

    Enemy/cannon: 0% -> 12.87% (add class shell and small matches)

---
+++ include/Enemy/Cannon.hpp
@@ -0,0 +1,152 @@
+#ifndef ENEMY_CANNON_HPP
+#define ENEMY_CANNON_HPP
+
+#include <Enemy/SmallEnemy.hpp>
+#include <MSound/MAnmSound.hpp>
+#include <Strategic/SharedParts.hpp>
+#include <JSystem/JGeometry.hpp>
+
+class SDLModelData;
+
+class TCannonSaveLoadParams : public TSmallEnemyParams {
+public:
+	TCannonSaveLoadParams(const char*);
+
+	/* 0x2D4 */ TParamRT<f32> mSLHideDist;
+	/* 0x2E8 */ TParamRT<f32> mSLBombDist;
+	/* 0x2FC */ TParamRT<f32> mSLKillerDist;
+	/* 0x310 */ TParamRT<s32> mSLBombInterval;
+	/* 0x324 */ TParamRT<s32> mSLKillerInterval;
+	/* 0x338 */ TParamRT<s32> mSLShootInterval;
+	/* 0x34C */ TParamRT<f32> mSLChorobeiAttackRadius;
+	/* 0x360 */ TParamRT<f32> mSLChorobeiAttackHeight;
+	/* 0x374 */ TParamRT<f32> mSLChorobeiDamageRadius;
+	/* 0x388 */ TParamRT<f32> mSLChorobeiDamageHeight;
+	/* 0x39C */ TParamRT<f32> mSLKillerTransYOffset;
+	/* 0x3B0 */ TParamRT<f32> mSLBombHeiGenerateRate;
+	/* 0x3C4 */ TParamRT<f32> mSLThrowXZSpeed;
+};
+
+class TCannon;
+
+class TCannonDom : public TSharedParts {
+public:
+	TCannonDom(TLiveActor*, int, SDLModelData*, u32, const char*);
+
+	virtual void perform(u32, JDrama::TGraphics*);
+
+public:
+	/* 0x1C */ MAnmSound* unk1C;
+	/* 0x20 */ const char* unk20;
+	/* 0x24 */ u8 unk24;
+	/* 0x25 */ u8 pad25[3];
+	/* 0x28 */ f32 unk28;
+	/* 0x2C */ f32 unk2C;
+	/* 0x30 */ f32 unk30;
+};
+
+class TChorobei : public THitActor {
+public:
+	TChorobei(TCannon*, int, const char*);
+
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+
+	BOOL checkHit();
+
+public:
+	/* 0x68 */ TCannon* unk68;
+	/* 0x6C */ TSharedParts* unk6C;
+	/* 0x70 */ f32 unk70;
+	/* 0x74 */ MAnmSound* unk74;
+	/* 0x78 */ const char* unk78;
+	/* 0x7C */ f32 unk7C;
+};
+
+class TCannon : public TSmallEnemy {
+public:
+	TCannon(const char*);
+
+	virtual void load(JSUMemoryInputStream&);
+	virtual void loadAfter();
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+	virtual MtxPtr getTakingMtx();
+	virtual void init(TLiveManager*);
+	virtual void calcRootMatrix();
+	virtual void moveObject();
+	virtual const char** getBasNameTable() const;
+	virtual void reset();
+	virtual bool isCollidMove(THitActor*) { return false; }
+	virtual BOOL isInhibitedForceMove() { return TRUE; }
+	virtual bool isHitValid(u32) { return false; }
+
+	void startChorobeiShout();
+	bool isObject();
+	void setKillerGoalPoint();
+	void killerShoot();
+	void bombShoot();
+	void bombSet();
+	bool isHitVallid(u32);
+
+	TCannonSaveLoadParams* getCannonParams() const
+	{
+		return (TCannonSaveLoadParams*)getSaveParam();
+	}
+
+	static u8 mChorobeiJntIdx;
+	static u8 mChorobeiHandJntIdx;
+	static f32 mVelocityRate;
+	static f32 mSearchRate;
+
+public:
+	/* 0x194 */ u8 pad194[0xC];
+	/* 0x1A0 */ THitActor* unk1A0;
+	/* 0x1A4 */ u8 pad1A4[4];
+	/* 0x1A8 */ TChorobei* unk1A8;
+	/* 0x1AC */ u8 pad1AC[0xC];
+	/* 0x1B8 */ TCannonDom* unk1B8;
+	/* 0x1BC */ u8 pad1BC[0x24];
+	/* 0x1E0 */ TCannonDom* unk1E0;
+	/* 0x1E4 */ TPosition3f unk1E4;
+	/* 0x214 */ void* unk214;
+	/* 0x218 */ void* unk218;
+	/* 0x21C */ bool unk21C;
+	/* 0x220 */ f32 unk220;
+	/* 0x224 */ f32 unk224;
+	/* 0x228 */ f32 unk228;
+	/* 0x22C */ f32 unk22C;
+	/* 0x230 */ bool unk230;
+	/* 0x231 */ u8 pad231[7];
+	/* 0x238 */ bool unk238;
+	/* 0x239 */ bool unk239;
+	/* 0x23A */ u8 pad23A[0x1A];
+	/* 0x254 */ void* unk254;
+	/* 0x258 */ void* unk258;
+	/* 0x25C */ JGeometry::TVec3<f32> unk25C[4];
+	/* 0x28C */ u8 pad28C[4];
+	/* 0x290 */ bool unk290;
+	/* 0x291 */ u8 pad291[0x1B];
+	/* 0x2AC */ f32 unk2AC;
+	/* 0x2B0 */ u8 pad2B0[4];
+};
+
+class TCannonManager : public TSmallEnemyManager {
+public:
+	TCannonManager(const char*);
+
+	virtual void load(JSUMemoryInputStream&);
+	virtual TSmallEnemy* createEnemyInstance();
+	virtual void clipEnemies(JDrama::TGraphics*) { }
+};
+
+DECLARE_NERVE(TNerveCannonObject, TLiveActor);
+DECLARE_NERVE(TNerveCannonDamageDemo, TLiveActor);
+DECLARE_NERVE(TNerveCannonDamage, TLiveActor);
+DECLARE_NERVE(TNerveCannonClose, TLiveActor);
+DECLARE_NERVE(TNerveCannonForceBombShoot, TLiveActor);
+DECLARE_NERVE(TNerveCannonShoot, TLiveActor);
+DECLARE_NERVE(TNerveCannonSearch, TLiveActor);
+DECLARE_NERVE(TNerveCannonOpen, TLiveActor);
+
+#endif
--- src/Enemy/cannon.cpp
+++ src/Enemy/cannon.cpp
@@ -1,1 +1,310 @@
+#include <Enemy/Cannon.hpp>
+#include <M3DUtil/InfectiousStrings.hpp>
+#include <M3DUtil/MActor.hpp>
+#include <MSound/MSSetSound.hpp>
+#include <MSound/MSoundBGM.hpp>
+#include <MSound/MSoundSE.hpp>
+#include <MarioUtil/MathUtil.hpp>
+#include <Strategic/ObjModel.hpp>
+#include <Strategic/Spine.hpp>
+#include <JSystem/J3D/J3DGraphAnimator/J3DAnimation.hpp>
+#include <JSystem/J3D/J3DGraphAnimator/J3DModel.hpp>
+#include <dolphin/mtx.h>
+#include <stdlib.h>
 
+static const char* cannon_bastable[] = {
+	nullptr,
+	nullptr,
+	"/scene/cannon/bas/CannonDom_break.bas",
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	"/scene/cannon/bas/tyorobe_appear1.bas",
+	"/scene/cannon/bas/tyorobe_damage1.bas",
+	"/scene/cannon/bas/tyorobe_down1.bas",
+	"/scene/cannon/bas/tyorobe_hyde1.bas",
+	"/scene/cannon/bas/tyorobe_throw1.bas",
+	nullptr,
+	nullptr,
+	nullptr,
+};
+
+static const char* sCannonDomPartsJointTable[] = {
+	"nullC",
+	"nullB",
+	"nullA",
+};
+
+u8 TCannon::mChorobeiJntIdx     = 4;
+u8 TCannon::mChorobeiHandJntIdx = 4;
+f32 TCannon::mVelocityRate      = 0.62f;
+f32 TCannon::mSearchRate        = 0.02f;
+
+DEFINE_NERVE(TNerveCannonObject, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveCannonDamageDemo, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveCannonDamage, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveCannonClose, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveCannonForceBombShoot, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveCannonShoot, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveCannonSearch, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveCannonOpen, TLiveActor) { return FALSE; }
+
+void TCannon::startChorobeiShout() { }
+
+bool TCannon::isObject()
+{
+	return mCurrentBckAnm == 2;
+}
+
+void TCannon::setKillerGoalPoint() { }
+
+void TCannon::killerShoot() { }
+
+void TCannon::bombShoot() { }
+
+void TCannon::bombSet() { }
+
+bool TCannon::isHitVallid(u32)
+{
+	return false;
+}
+
+MtxPtr TCannon::getTakingMtx()
+{
+	if (checkLiveFlag(LIVE_FLAG_UNK4000))
+		return unk1A8->unk6C->getMActor()->getModel()->getBaseTRMtx();
+
+	MtxPtr takingMtx = unk1A8->unk6C->getMActor()->getModel()->getAnmMtx(
+	    mChorobeiHandJntIdx);
+	unk1E4.identity33();
+	unk1E4.mMtx[0][3] = unk1A8->mPosition.x;
+	unk1E4.mMtx[1][3] = takingMtx[1][3];
+	unk1E4.mMtx[2][3] = unk1A8->mPosition.z;
+	return unk1E4.mMtx;
+}
+
+void TCannon::calcRootMatrix()
+{
+	TSmallEnemy::calcRootMatrix();
+}
+
+void TCannon::perform(u32 flags, JDrama::TGraphics* graphics)
+{
+	TSmallEnemy::perform(flags, graphics);
+}
+
+const char** TCannon::getBasNameTable() const
+{
+	return cannon_bastable;
+}
+
+BOOL TCannon::receiveMessage(THitActor* sender, u32 message)
+{
+	if (sender->mActorType == 0x40000235 && message == HIT_MESSAGE_TAKE
+	    && mHolder == nullptr) {
+		mHolder = (TTakeActor*)sender;
+		return TRUE;
+	}
+
+	if (message == HIT_MESSAGE_SPRAYED_BY_WATER)
+		return TRUE;
+
+	return FALSE;
+}
+
+void TCannon::moveObject()
+{
+	TSmallEnemy::moveObject();
+}
+
+void TCannon::reset()
+{
+	TSmallEnemy::reset();
+	unk1A0 = nullptr;
+	unk1A8 = nullptr;
+	unk1B8 = nullptr;
+	unk1E0 = nullptr;
+	unk214 = nullptr;
+	unk218 = nullptr;
+	unk21C = false;
+	unk220 = 0.0f;
+	unk224 = 0.0f;
+	unk228 = 0.0f;
+	unk22C = 0.0f;
+	unk230 = true;
+	unk238 = false;
+	unk239 = true;
+	unk254 = nullptr;
+	unk258 = nullptr;
+	unk290 = false;
+	unk2AC = 0.0f;
+}
+
+void TCannon::init(TLiveManager* manager)
+{
+	TSmallEnemy::init(manager);
+}
+
+void TCannon::loadAfter()
+{
+	TSmallEnemy::loadAfter();
+}
+
+void TCannon::load(JSUMemoryInputStream& stream)
+{
+	TSmallEnemy::load(stream);
+}
+
+TCannon::TCannon(const char* name)
+    : TSmallEnemy(name)
+    , unk1A0(nullptr)
+    , unk1A8(nullptr)
+    , unk1B8(nullptr)
+    , unk1E0(nullptr)
+    , unk214(nullptr)
+    , unk218(nullptr)
+    , unk21C(false)
+    , unk220(0.0f)
+    , unk230(true)
+    , unk238(false)
+    , unk239(true)
+    , unk254(nullptr)
+    , unk258(nullptr)
+    , unk290(false)
+    , unk2AC(0.0f)
+{
+	unk224 = 0.0f;
+	unk228 = 0.0f;
+	unk22C = 0.0f;
+}
+
+void TCannonDom::perform(u32 flags, JDrama::TGraphics* graphics)
+{
+	if ((unk10->mLiveFlag & 0x7) != 0)
+		return;
+
+	if (flags == 2) {
+		if (unk1C != nullptr && unk20 != nullptr) {
+			J3DFrameCtrl* ctrl = unk18->getFrameCtrl(0);
+			unk1C->animeLoop((Vec*)&unk10->mPosition, ctrl->getFrame(),
+			                 ctrl->getRate(), 0, 4);
+		}
+
+		Mtx rot;
+		MtxPtr connectedMtx = getConnectedMtx();
+		MsMtxSetRotRPH(rot, unk28, unk2C, 0.0f);
+		PSMTXConcat(connectedMtx, rot, connectedMtx);
+		PSMTXCopy(connectedMtx, unk18->getModel()->getBaseTRMtx());
+	}
+
+	unk18->perform(flags, graphics);
+}
+
+TCannonDom::TCannonDom(TLiveActor* owner, int jointIndex, SDLModelData* data,
+                       u32 flags, const char* name)
+    : TSharedParts(owner, jointIndex, data, flags, name)
+    , unk1C(nullptr)
+    , unk20(nullptr)
+    , unk24(0)
+    , unk28(0.0f)
+    , unk2C(0.0f)
+    , unk30(0.0f)
+{
+	f32 min = 0.0f;
+	f32 max = 360.0f;
+	unk30  = min + (max - min) * ((f32)rand() * (1.0f / 32768.0f));
+
+	if (unk1C != nullptr)
+		return;
+
+	unk1C = new MAnmSound(gpMSound);
+	unk1C->initAnmSound(nullptr, 1, 0.0f);
+}
+
+BOOL TChorobei::receiveMessage(THitActor*, u32)
+{
+	return FALSE;
+}
+
+BOOL TChorobei::checkHit()
+{
+	return FALSE;
+}
+
+void TChorobei::perform(u32 flags, JDrama::TGraphics* graphics)
+{
+	if (unk6C != nullptr)
+		unk6C->perform(flags, graphics);
+
+	if (unk74 != nullptr && unk78 != nullptr) {
+		J3DFrameCtrl* ctrl = unk6C->getMActor()->getFrameCtrl(0);
+		unk74->animeLoop(&mPosition, ctrl->getFrame(), ctrl->getRate(), 0, 4);
+	}
+}
+
+TChorobei::TChorobei(TCannon* cannon, int jointIndex, const char* name)
+    : THitActor(name)
+    , unk68(cannon)
+    , unk6C(nullptr)
+    , unk70(0.0f)
+    , unk74(nullptr)
+    , unk78(nullptr)
+    , unk7C(300.0f)
+{
+	unk6C = new TSharedParts(cannon, jointIndex,
+	                         "/scene/cannon/tyorobe_model1.bmd", 0x10020000,
+	                         3, "<TSharedParts>");
+	if (unk74 != nullptr)
+		return;
+
+	unk74 = new MAnmSound(gpMSound);
+	unk74->initAnmSound(nullptr, 1, 0.0f);
+}
+
+TSmallEnemy* TCannonManager::createEnemyInstance()
+{
+	return new TCannon("砲台");
+}
+
+void TCannonManager::load(JSUMemoryInputStream& stream)
+{
+	TSmallEnemyManager::load(stream);
+	unk38 = new TCannonSaveLoadParams("/enemy/cannon.prm");
+}
+
+TCannonManager::TCannonManager(const char* name)
+    : TSmallEnemyManager(name)
+{
+}
+
+TCannonSaveLoadParams::TCannonSaveLoadParams(const char* path)
+    : TSmallEnemyParams(path)
+    , PARAM_INIT(mSLHideDist, 300.0f)
+    , PARAM_INIT(mSLBombDist, 2000.0f)
+    , PARAM_INIT(mSLKillerDist, 10000.0f)
+    , PARAM_INIT(mSLBombInterval, 100)
+    , PARAM_INIT(mSLKillerInterval, 50)
+    , PARAM_INIT(mSLShootInterval, 100)
+    , PARAM_INIT(mSLChorobeiAttackRadius, 100.0f)
+    , PARAM_INIT(mSLChorobeiAttackHeight, 100.0f)
+    , PARAM_INIT(mSLChorobeiDamageRadius, 100.0f)
+    , PARAM_INIT(mSLChorobeiDamageHeight, 100.0f)
+    , PARAM_INIT(mSLKillerTransYOffset, -50.0f)
+    , PARAM_INIT(mSLBombHeiGenerateRate, 0.7f)
+    , PARAM_INIT(mSLThrowXZSpeed, 12.0f)
+{
+	TParams::load(mPrmPath);
+}
