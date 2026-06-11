3bc4fdd9117d677191e11122291664b3b86f3c19
trader-vps <trader-vps@btc-predictor>
2026-06-11 07:56:02 +0000

    Enemy/bosseel: 0% -> manager and params setup

--- include/Enemy/BossEel.hpp
+++ include/Enemy/BossEel.hpp
@@ -6,20 +6,103 @@
 #include <Strategic/Nerve.hpp>
 
 class TLiveActor;
+class TBEelTearsDrop;
+class SDLModelData;
+
+struct TBossEelUnk1EC {
+	s32 unk0;
+	s32 unk4;
+};
 
 class TBossEel : public TSpineEnemy {
 public:
 	TBossEel(const char*);
 	virtual ~TBossEel();
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+	virtual void init(TLiveManager*);
+	virtual BOOL hasMapCollision() const;
+	virtual const char** getBasNameTable() const;
 
-	/* 0x150 */ u8 unk150[0xD0];
+	static f32 mOpenRollSpeed;
+	static u8 mUseObjCollision;
+	static f32 mForcePow;
+	static u8 mUseMapCollision;
+
+	/* 0x150 */ u8 unk150[0x58];
+	/* 0x1A8 */ void* unk1A8;
+	/* 0x1AC */ void* unk1AC;
+	/* 0x1B0 */ void* unk1B0;
+	/* 0x1B4 */ u8 unk1B4[0x8];
+	/* 0x1BC */ f32 unk1BC;
+	/* 0x1C0 */ void* unk1C0;
+	/* 0x1C4 */ void* unk1C4;
+	/* 0x1C8 */ u32 unk1C8;
+	/* 0x1CC */ f32 unk1CC;
+	/* 0x1D0 */ u8 unk1D0;
+	/* 0x1D1 */ u8 unk1D1[0x3];
+	/* 0x1D4 */ f32 unk1D4;
+	/* 0x1D8 */ f32 unk1D8;
+	/* 0x1DC */ u8 unk1DC[0xC];
+	/* 0x1E8 */ void* unk1E8;
+	/* 0x1EC */ TBossEelUnk1EC* unk1EC;
+	/* 0x1F0 */ u8 unk1F0;
+	/* 0x1F1 */ u8 unk1F1[0x3];
+	/* 0x1F4 */ f32 unk1F4;
+	/* 0x1F8 */ u32 unk1F8;
+	/* 0x1FC */ u8 unk1FC;
+	/* 0x1FD */ u8 unk1FD;
+	/* 0x1FE */ u8 unk1FE;
+	/* 0x1FF */ u8 unk1FF;
+	/* 0x200 */ void* unk200;
+	/* 0x204 */ u8 unk204[0xC];
+	/* 0x210 */ void* unk210;
+	/* 0x214 */ void* unk214;
+	/* 0x218 */ void* unk218;
+	/* 0x21C */ u8 unk21C;
+	/* 0x21D */ u8 unk21D;
+	/* 0x21E */ u8 unk21E[0x2];
 };
 
-class TBossEelSaveParams {
+class TBossEelSaveParams : public TParams {
 public:
 	TBossEelSaveParams();
 
-	/* 0x000 */ u8 unk0[0x2C4];
+	/* 0x008 */ TParamRT<f32> mSLInitTransYOffset;
+	/* 0x01C */ TParamRT<f32> mSLAppearMoveDistY;
+	/* 0x030 */ TParamRT<f32> mSLBodyScale;
+	/* 0x044 */ TParamRT<f32> mSLViewClipFar;
+	/* 0x058 */ TParamRT<f32> mSLViewClipRadius;
+	/* 0x06C */ TParamRT<f32> mSLBodyToHeadDistance;
+	/* 0x080 */ TParamRT<f32> mSLBodyAttackRadius;
+	/* 0x094 */ TParamRT<f32> mSLBodyAttackHeight;
+	/* 0x0A8 */ TParamRT<f32> mSLBodyDamageRadius;
+	/* 0x0BC */ TParamRT<f32> mSLBodyDamageHeight;
+	/* 0x0D0 */ TParamRT<f32> mSLHeadAttackRadius;
+	/* 0x0E4 */ TParamRT<f32> mSLHeadAttackHeight;
+	/* 0x0F8 */ TParamRT<f32> mSLHeadDamageRadius;
+	/* 0x10C */ TParamRT<f32> mSLHeadDamageHeight;
+	/* 0x120 */ TParamRT<f32> mSLToothAttackRadius;
+	/* 0x134 */ TParamRT<f32> mSLToothAttackHeight;
+	/* 0x148 */ TParamRT<f32> mSLToothDamageRadius;
+	/* 0x15C */ TParamRT<f32> mSLToothDamageHeight;
+	/* 0x170 */ TParamRT<f32> mSLSpinAccel;
+	/* 0x184 */ TParamRT<f32> mSLSpinMaxSpeed;
+	/* 0x198 */ TParamRT<f32> mSLToothUpSpeed;
+	/* 0x1AC */ TParamRT<f32> mSLToothLiveHeight;
+	/* 0x1C0 */ TParamRT<s32> mSLToothMaxHitPoint;
+	/* 0x1D4 */ TParamRT<s32> mSLGenTearsTime;
+	/* 0x1E8 */ TParamRT<f32> mSLVortexAttackRadius;
+	/* 0x1FC */ TParamRT<f32> mSLVortexAttackHeight;
+	/* 0x210 */ TParamRT<f32> mSLVortexDamageRadius;
+	/* 0x224 */ TParamRT<f32> mSLVortexDamageHeight;
+	/* 0x238 */ TParamRT<s32> mSLVortexLiveTimer;
+	/* 0x24C */ TParamRT<f32> mSLVortexScaleXZ;
+	/* 0x260 */ TParamRT<f32> mSLVortexScaleY;
+	/* 0x274 */ TParamRT<s32> mSLMouthOpenFrame;
+	/* 0x288 */ TParamRT<s32> mSLMouthOpenInterval;
+	/* 0x29C */ TParamRT<s32> mSLCanEatFrame;
+	/* 0x2B0 */ TParamRT<f32> mSLBreathInPower;
 };
 
 class TBossEelManager : public TEnemyManager {
@@ -31,23 +114,79 @@ public:
 	}
 
 	virtual ~TBossEelManager();
+	virtual void loadAfter();
+	virtual void createModelData();
+	virtual void clipEnemies(JDrama::TGraphics*);
 
 	/* 0x054 */ TBossEelSaveParams mSaveParams;
+};
+
+class TBEelTearsSaveLoadParams : public TSpineEnemyParams {
+public:
+	TBEelTearsSaveLoadParams(const char*);
+
+	/* 0x0A8 */ TParamRT<f32> mSLTearsUpSpeed;
+	/* 0x0BC */ TParamRT<f32> mSLTearsDamageUpSpeed;
+	/* 0x0D0 */ TParamRT<f32> mSLTearsLiveHeight;
+	/* 0x0E4 */ TParamRT<s32> mSLTearsSplitNum;
+	/* 0x0F8 */ TParamRT<s32> mSLTearsDamageRadius;
+	/* 0x10C */ TParamRT<s32> mSLTearsDamageHeight;
+	/* 0x120 */ TParamRT<s32> mSLTearsAttackRadius;
+	/* 0x134 */ TParamRT<s32> mSLTearsAttackHeight;
+	/* 0x148 */ TParamRT<s32> mSLTearsDropDamageRadius;
+	/* 0x15C */ TParamRT<s32> mSLTearsDropDamageHeight;
+	/* 0x170 */ TParamRT<s32> mSLTearsDropAttackRadius;
+	/* 0x184 */ TParamRT<s32> mSLTearsDropAttackHeight;
+	/* 0x198 */ TParamRT<f32> mSLHighPolyDistY;
+	/* 0x1AC */ TParamRT<f32> mSLHitAnmFrameRate;
+	/* 0x1C0 */ TParamRT<f32> mSLBodyScaleLow;
+	/* 0x1D4 */ TParamRT<f32> mSLBodyScaleHigh;
+	/* 0x1E8 */ TParamRT<f32> mSLTearsDropScaleLow;
+	/* 0x1FC */ TParamRT<f32> mSLTearsDropScaleHigh;
+	/* 0x210 */ f32 mBodyScaleLow;
+	/* 0x214 */ f32 mBodyScaleHigh;
+	/* 0x218 */ f32 mTearsDropScaleLow;
+	/* 0x21C */ f32 mTearsDropScaleHigh;
 };
 
 class TBEelTears : public TSpineEnemy {
 public:
 	TBEelTears(const char*);
+	virtual const char** getBasNameTable() const;
 
-	/* 0x150 */ u8 unk150[0x20];
+	/* 0x150 */ u8 unk150[0xC];
+	/* 0x15C */ TBEelTearsSaveLoadParams* unk15C;
+	/* 0x160 */ u8 unk160;
+	/* 0x161 */ u8 unk161[0x3];
+	/* 0x164 */ s32 unk164;
+	/* 0x168 */ void* unk168;
+	/* 0x16C */ void* unk16C;
 };
 
 class TBEelTearsManager : public TEnemyManager {
 public:
 	TBEelTearsManager(const char*);
 	virtual ~TBEelTearsManager();
+	virtual void load(JSUMemoryInputStream&);
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual TSpineEnemy* createEnemyInstance();
+	virtual void createEnemies(int);
+	virtual void createModelData();
+	virtual void loadAfter();
 
-	/* 0x054 */ u8 unk54[0x78];
+	/* 0x054 */ TBEelTearsDrop* mDrops[30];
+};
+
+class TBEelTearsDrop : public THitActor {
+public:
+	TBEelTearsDrop(TBEelTears*, int, SDLModelData*, const char*);
+	virtual void perform(u32, JDrama::TGraphics*);
+
+	/* 0x068 */ void* unk68;
+	/* 0x06C */ u8 unk6C;
+	/* 0x06D */ u8 unk6D[0x3];
+	/* 0x070 */ f32 unk70;
+	/* 0x074 */ TBEelTears* unk74;
 };
 
 class TOilBall : public TBEelTears {
--- src/Enemy/bosseel.cpp
+++ src/Enemy/bosseel.cpp
@@ -1,1 +1,265 @@
+#include <Enemy/BossEel.hpp>
+#include <Strategic/ObjManager.hpp>
+#include <System/Particles.hpp>
 
+// rogue includes needed for matching sinit & rodata
+#include <M3DUtil/InfectiousStrings.hpp>
+#include <MSound/MSSetSound.hpp>
+#include <MSound/MSoundBGM.hpp>
+
+f32 TBossEel::mOpenRollSpeed = 0.3f;
+u8 TBossEel::mUseObjCollision = TRUE;
+f32 TBossEel::mForcePow       = 10.0f;
+u8 TBossEel::mUseMapCollision;
+
+static const char* bosseel_bastable[] = {
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	"/scene/bosseel/bas/meoto_in_loop.bas",
+	nullptr,
+	"/scene/bosseel/bas/meoto_mogu.bas",
+	nullptr,
+	nullptr,
+	nullptr,
+	"/scene/bosseel/bas/meoto_out_loop.bas",
+	"/scene/bosseel/bas/meoto_paku.bas",
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+};
+
+static const char* bossEelTears_bastable[] = {
+	nullptr,
+	nullptr,
+	nullptr,
+	nullptr,
+};
+
+TBossEel::TBossEel(const char* name)
+    : TSpineEnemy(name)
+    , unk1A8(nullptr)
+    , unk1AC(nullptr)
+    , unk1B0(nullptr)
+    , unk1BC(1.0f)
+    , unk1C0(nullptr)
+    , unk1C4(nullptr)
+    , unk1CC(500.0f)
+    , unk1D0(TRUE)
+    , unk1D4(2350.0f)
+    , unk1D8(0.75f)
+    , unk1E8(nullptr)
+    , unk1F0(FALSE)
+    , unk1F4(0.0f)
+    , unk1FC(FALSE)
+    , unk1FD(FALSE)
+    , unk1FE(FALSE)
+    , unk200(nullptr)
+    , unk210(nullptr)
+    , unk214(nullptr)
+    , unk218(nullptr)
+    , unk21C(FALSE)
+    , unk21D(TRUE)
+{
+	TBossEelUnk1EC* unk = new TBossEelUnk1EC;
+	if (unk != nullptr) {
+		unk->unk0 = 0;
+		unk->unk4 = 1;
+	}
+	unk1EC = unk;
+}
+
+BOOL TBossEel::receiveMessage(THitActor*, u32) { return FALSE; }
+
+BOOL TBossEel::hasMapCollision() const { return TRUE; }
+
+const char** TBossEel::getBasNameTable() const { return bosseel_bastable; }
+
+TBossEelSaveParams::TBossEelSaveParams()
+    : TParams("/enemy/bosseel.prm")
+    , PARAM_INIT(mSLInitTransYOffset, 0.0f)
+    , PARAM_INIT(mSLAppearMoveDistY, 6000.0f)
+    , PARAM_INIT(mSLBodyScale, 0.75f)
+    , PARAM_INIT(mSLViewClipFar, 25000.0f)
+    , PARAM_INIT(mSLViewClipRadius, 12000.0f)
+    , PARAM_INIT(mSLBodyToHeadDistance, 6000.0f)
+    , PARAM_INIT(mSLBodyAttackRadius, 2400.0f)
+    , PARAM_INIT(mSLBodyAttackHeight, 6300.0f)
+    , PARAM_INIT(mSLBodyDamageRadius, 2200.0f)
+    , PARAM_INIT(mSLBodyDamageHeight, 6100.0f)
+    , PARAM_INIT(mSLHeadAttackRadius, 3600.0f)
+    , PARAM_INIT(mSLHeadAttackHeight, 5600.0f)
+    , PARAM_INIT(mSLHeadDamageRadius, 3400.0f)
+    , PARAM_INIT(mSLHeadDamageHeight, 5400.0f)
+    , PARAM_INIT(mSLToothAttackRadius, 500.0f)
+    , PARAM_INIT(mSLToothAttackHeight, 300.0f)
+    , PARAM_INIT(mSLToothDamageRadius, 600.0f)
+    , PARAM_INIT(mSLToothDamageHeight, 400.0f)
+    , PARAM_INIT(mSLSpinAccel, 0.01f)
+    , PARAM_INIT(mSLSpinMaxSpeed, 10.0f)
+    , PARAM_INIT(mSLToothUpSpeed, 5.0f)
+    , PARAM_INIT(mSLToothLiveHeight, 2000.0f)
+    , PARAM_INIT(mSLToothMaxHitPoint, 50)
+    , PARAM_INIT(mSLGenTearsTime, 100)
+    , PARAM_INIT(mSLVortexAttackRadius, 500.0f)
+    , PARAM_INIT(mSLVortexAttackHeight, 300.0f)
+    , PARAM_INIT(mSLVortexDamageRadius, 600.0f)
+    , PARAM_INIT(mSLVortexDamageHeight, 400.0f)
+    , PARAM_INIT(mSLVortexLiveTimer, 300)
+    , PARAM_INIT(mSLVortexScaleXZ, 1.0f)
+    , PARAM_INIT(mSLVortexScaleY, 1.0f)
+    , PARAM_INIT(mSLMouthOpenFrame, 500)
+    , PARAM_INIT(mSLMouthOpenInterval, 2000)
+    , PARAM_INIT(mSLCanEatFrame, 200)
+    , PARAM_INIT(mSLBreathInPower, 10.0f)
+{
+	TParams::load(mPrmPath);
+}
+
+void TBossEelManager::clipEnemies(JDrama::TGraphics* graphics)
+{
+	clipActorsAux(graphics, mSaveParams.mSLViewClipFar.get(),
+	              mSaveParams.mSLViewClipRadius.get());
+}
+
+void TBossEelManager::createModelData()
+{
+	static const TModelDataLoadEntry entry[] = {
+		{ "meoto_model.bmd", 0x10020000, 0 },
+		{ nullptr, 0, 0 },
+	};
+	createModelDataArray(entry);
+}
+
+#define LOAD_BOSSEEL_PARTICLE(id, path)                                        \
+	do {                                                                       \
+		if (!gParticleFlagLoaded[id]) {                                        \
+			gpResourceManager->load(path, id);                                 \
+			gParticleFlagLoaded[id] = true;                                    \
+		}                                                                      \
+	} while (0)
+
+void TBossEelManager::loadAfter()
+{
+	LOAD_BOSSEEL_PARTICLE(0xd3, "/scene/bosseel/jpa/ms_meo_awa_tooth.jpa");
+	LOAD_BOSSEEL_PARTICLE(0xd4, "/scene/bosseel/jpa/ms_meo_awa_mouth.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x192, "/scene/bosseel/jpa/ms_meo_eyeblur.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x193, "/scene/bosseel/jpa/ms_meo_spin_smoke.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x194, "/scene/bosseel/jpa/ms_meo_spin_smoke_l.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x195, "/scene/bosseel/jpa/ms_meo_spin_awa.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x196, "/scene/bosseel/jpa/ms_meo_spin_awa_l.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x197, "/scene/bosseel/jpa/ms_meo_awa_body.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x198, "/scene/bosseel/jpa/ms_meo_awa_dead.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x199, "/scene/bosseel/jpa/ms_meo_suikomi.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x19a, "/scene/bosseel/jpa/ms_meo_tooth_wash.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x19b, "/scene/bosseel/jpa/ms_meo_tooth_kira.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x19c, "/scene/bosseel/jpa/ms_meo_tooth_always.jpa");
+}
+
+TBEelTears::TBEelTears(const char* name)
+    : TSpineEnemy(name)
+    , unk15C(nullptr)
+    , unk160(TRUE)
+    , unk164(0)
+    , unk168(nullptr)
+    , unk16C(nullptr)
+{
+}
+
+const char** TBEelTears::getBasNameTable() const
+{
+	return bossEelTears_bastable;
+}
+
+TBEelTearsManager::TBEelTearsManager(const char* name)
+    : TEnemyManager(name)
+{
+}
+
+void TBEelTearsManager::load(JSUMemoryInputStream& stream)
+{
+	unk38 = new TBEelTearsSaveLoadParams("/enemy/bossEelTears.prm");
+	TEnemyManager::load(stream);
+}
+
+void TBEelTearsManager::perform(u32 flags, JDrama::TGraphics* graphics)
+{
+	if (flags & 2)
+		clipEnemies(graphics);
+
+	for (int i = 0; i < getActiveObjNum(); ++i)
+		getObj(i)->perform(flags, graphics);
+
+	for (int i = 0; i < 30; ++i) {
+		if (mDrops[i]->unk6C)
+			mDrops[i]->perform(flags, graphics);
+	}
+}
+
+TSpineEnemy* TBEelTearsManager::createEnemyInstance()
+{
+	return new TBEelTears("めおとウナギ涙");
+}
+
+void TBEelTearsManager::createModelData()
+{
+	static const TModelDataLoadEntry entry[] = {
+		{ "tears.bmd", 0x11240000, 0 },
+		{ "tears_waterhit.bmd", 0x11240000, 0 },
+		{ nullptr, 0, 0 },
+	};
+	createModelDataArray(entry);
+}
+
+void TBEelTearsManager::loadAfter()
+{
+	LOAD_BOSSEEL_PARTICLE(0xd5,
+	                      "/scene/bossEelTears/ms_meo_tear_bomb.jpa");
+	LOAD_BOSSEEL_PARTICLE(0xd6,
+	                      "/scene/bossEelTears/ms_meo_tear_awaget.jpa");
+	LOAD_BOSSEEL_PARTICLE(0x19d,
+	                      "/scene/bossEelTears/ms_meo_tear_awa.jpa");
+}
+
+#undef LOAD_BOSSEEL_PARTICLE
+
+TBEelTearsSaveLoadParams::TBEelTearsSaveLoadParams(const char* path)
+    : TSpineEnemyParams(path)
+    , PARAM_INIT(mSLTearsUpSpeed, 5.0f)
+    , PARAM_INIT(mSLTearsDamageUpSpeed, 1.0f)
+    , PARAM_INIT(mSLTearsLiveHeight, 2000.0f)
+    , PARAM_INIT(mSLTearsSplitNum, 2)
+    , PARAM_INIT(mSLTearsDamageRadius, 600)
+    , PARAM_INIT(mSLTearsDamageHeight, 400)
+    , PARAM_INIT(mSLTearsAttackRadius, 500)
+    , PARAM_INIT(mSLTearsAttackHeight, 300)
+    , PARAM_INIT(mSLTearsDropDamageRadius, 600)
+    , PARAM_INIT(mSLTearsDropDamageHeight, 400)
+    , PARAM_INIT(mSLTearsDropAttackRadius, 500)
+    , PARAM_INIT(mSLTearsDropAttackHeight, 300)
+    , PARAM_INIT(mSLHighPolyDistY, 5.0f)
+    , PARAM_INIT(mSLHitAnmFrameRate, 1.0f)
+    , PARAM_INIT(mSLBodyScaleLow, 1.0f)
+    , PARAM_INIT(mSLBodyScaleHigh, 1.0f)
+    , PARAM_INIT(mSLTearsDropScaleLow, 1.0f)
+    , PARAM_INIT(mSLTearsDropScaleHigh, 1.0f)
+    , mBodyScaleLow(0.0f)
+    , mBodyScaleHigh(1.0f)
+    , mTearsDropScaleLow(0.0f)
+    , mTearsDropScaleHigh(1.0f)
+{
+	TParams::load(mPrmPath);
+	mBodyScaleLow       = mSLBodyScaleLow.get();
+	mBodyScaleHigh      = mSLBodyScaleHigh.get();
+	mTearsDropScaleLow  = mSLTearsDropScaleLow.get();
+	mTearsDropScaleHigh = mSLTearsDropScaleHigh.get();
+}
