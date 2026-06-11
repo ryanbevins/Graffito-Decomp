bfb2b4cffd4d7184a30cfc866e1316f271a5f18b
trader-vps <trader-vps@btc-predictor>
2026-06-11 17:31:25 +0000

    Player/MarioRecord: certify Equivalent (stack-frame-only init diff)

--- configure.py
+++ configure.py
@@ -915,7 +915,7 @@ config.libs = [
             Object(NonMatching, "Player/MarioMain.cpp"),
             Object(NonMatching, "Player/MarioMove.cpp"),
             Object(NonMatching, "Player/MarioPhysics.cpp"),
-            Object(NonMatching, "Player/MarioRecord.cpp"),
+            Object(Equivalent, "Player/MarioRecord.cpp"),
             Object(NonMatching, "Player/MarioRun.cpp"),
             Object(NonMatching, "Player/MarioSpecial.cpp"),
             Object(NonMatching, "Player/MarioUpper.cpp"),
