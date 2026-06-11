78f27723acf845da1d4ded653211525b17fdbf22
trader-vps <trader-vps@btc-predictor>
2026-06-11 14:13:21 +0000

    JParticle/JPAParticle: certify Equivalent (stack-only child predicate diff)

--- configure.py
+++ configure.py
@@ -521,7 +521,7 @@ config.libs = [
             Object(NonMatching, "JSystem/JParticle/JPAEmitterManager.cpp"),
             Object(NonMatching, "JSystem/JParticle/JPAField.cpp"),
             Object(NonMatching, "JSystem/JParticle/JPAMath.cpp"),
-            Object(NonMatching, "JSystem/JParticle/JPAParticle.cpp"),
+            Object(Equivalent, "JSystem/JParticle/JPAParticle.cpp"),
             Object(Matching, "JSystem/JParticle/JPADrawSetupTev.cpp"),
             Object(NonMatching, "JSystem/JParticle/JPADraw.cpp"),
             Object(NonMatching, "JSystem/JParticle/JPADrawVisitor.cpp"),
