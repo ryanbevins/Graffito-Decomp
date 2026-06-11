18feab94351887f6d0c9e20a0f9f35a56dcb3b33
trader-vps <trader-vps@btc-predictor>
2026-06-11 19:15:19 +0000

    Enemy/bosstelesa init: 80.3% -> 81.6% (reload model data for texture swap)

--- src/Enemy/bosstelesa.cpp
+++ src/Enemy/bosstelesa.cpp
@@ -412,7 +412,8 @@ void TBossTelesa::init(TLiveManager* manager)
 	    = JDrama::TNameRefGen::search<JDrama::TViewObj>("スクリーンテクスチャ");
 	void* dataOwner = *(void**)((u8*)screenTex + 0x10);
 	ResTIMG* texture = *(ResTIMG**)((u8*)dataOwner + 0x20);
-	SMS_ChangeTextureAll(model->getModelData(), "H_ma_rak_dummy", *texture);
+	SMS_ChangeTextureAll(mMActor->unk4->mModelData, "H_ma_rak_dummy",
+	                     *texture);
 
 	unk358 = SMS_GetMarioHP();
 }
