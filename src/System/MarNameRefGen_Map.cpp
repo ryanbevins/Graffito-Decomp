4175888e160c25c5384e60ecaec61fabc1a84f2d
trader-vps <trader-vps@btc-predictor>
2026-06-12 06:10:06 +0000

    System/MarNameRefGen_Map: align rodata literals

--- src/System/MarNameRefGen_Map.cpp
+++ src/System/MarNameRefGen_Map.cpp
@@ -1,5 +1,4 @@
 #include <System/MarNameRefGen.hpp>
-#include <M3DUtil/InfectiousStrings.hpp>
 #include <Map/Map.hpp>
 #include <Map/MapDraw.hpp>
 #include <Map/Sky.hpp>
@@ -11,6 +10,17 @@
 #include <Map/MarineSnow.hpp>
 #include <Map/StickyStainManager.hpp>
 #include <Map/BathWaterManager.hpp>
+
+static const char dummyMactorStringValue1[] = "\0\0\0\0\0\0\0\0\0\0\0";
+static const char SMS_NO_MEMORY_MESSAGE[]   = "メモリが足りません\n";
+static const char MtxCalcTypeName0[]
+    = "MActorMtxCalcType_Basic クラシックスケールＯＮ";
+static const char MtxCalcTypeName1[]
+    = "MActorMtxCalcType_Softimage クラシックスケールＯＦＦ";
+static const char MtxCalcTypeName2[]
+    = "MActorMtxCalcType_MotionBlend モーションブレンド";
+static const char MtxCalcTypeName3[]
+    = "MActorMtxCalcType_User ユーザー定義";
 
 const char* cSunVolumeName    = "/scene/sun";
 const char* cSunsetVolumeName = "/scene/sunset";
