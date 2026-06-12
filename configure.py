585738e4e144bbf0b11b8b380ea246c5cd8ba108
trader-vps <trader-vps@btc-predictor>
2026-06-12 18:48:16 +0000

    GC2D/SelectMenu: certify Equivalent (source link, UI codegen only)

--- configure.py
+++ configure.py
@@ -1102,7 +1102,7 @@ config.libs = [
             Object(NonMatching, "GC2D/CardSave.cpp"),
             Object(NonMatching, "GC2D/CardLoad.cpp"),
             Object(NonMatching, "GC2D/ConsoleStr.cpp"),
-            Object(NonMatching, "GC2D/SelectMenu.cpp"),
+            Object(Equivalent, "GC2D/SelectMenu.cpp"),
             Object(Equivalent, "GC2D/SelectDir.cpp"),
             Object(NonMatching, "GC2D/SelectShine2.cpp"),
             Object(Equivalent, "GC2D/BlendPane.cpp"),
