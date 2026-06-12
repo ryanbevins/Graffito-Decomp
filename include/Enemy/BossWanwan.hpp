6abfa897d68ec5462a5aa00d6aca0c53caa25b88
trader-vps <trader-vps@btc-predictor>
2026-06-12 08:34:18 +0000

    Enemy/bosswanwan: 0% -> 19.35% (recover ABI scaffold)

--- include/Enemy/BossWanwan.hpp
+++ include/Enemy/BossWanwan.hpp
@@ -3,22 +3,181 @@
 
 #include <Enemy/Enemy.hpp>
 #include <Enemy/EnemyManager.hpp>
+#include <JSystem/JDrama/JDRViewObj.hpp>
+#include <M3DUtil/M3UJoint.hpp>
+#include <Strategic/Binder.hpp>
 #include <Strategic/Nerve.hpp>
 
 class TLiveActor;
+class MActor;
+class TBossWanwan;
+
+class TBWParams : public TSpineEnemyParams {
+public:
+	TBWParams(const char*);
+
+	/* 0x0A8 */ TParamRT<f32> mSLMarchSpeed;
+	/* 0x0BC */ TParamRT<f32> mSLTurnSpeed;
+	/* 0x0D0 */ TParamRT<f32> mSLLeashNodeLen;
+	/* 0x0E4 */ TParamRT<f32> mSLPicketHeight;
+	/* 0x0F8 */ TParamRT<f32> mSLPicketRadius;
+	/* 0x10C */ TParamRT<f32> mSLChainHitHeight;
+	/* 0x120 */ TParamRT<f32> mSLChainHitRadius;
+	/* 0x134 */ TParamRT<f32> mSLChainGroundRadius;
+	/* 0x148 */ TParamRT<f32> mSLPullLimit;
+	/* 0x15C */ TParamRT<f32> mSLAttackSpeed;
+	/* 0x170 */ TParamRT<s32> mSLStunTimer;
+	/* 0x184 */ TParamRT<f32> mSLSearchLength;
+	/* 0x198 */ TParamRT<f32> mSLSearchAngle;
+	/* 0x1AC */ TParamRT<u8> mSLBWHitPointMax;
+	/* 0x1C0 */ TParamRT<f32> mSLHeadGap;
+	/* 0x1D4 */ TParamRT<f32> mSLShakeLengthMax;
+	/* 0x1E8 */ TParamRT<f32> mSLShakeLengthMaxHP0;
+};
+
+class TBossWanwanMtxCalc : public M3UMtxCalcSIAnmBlendQuat {
+public:
+	TBossWanwanMtxCalc(TBossWanwan* owner)
+	    : M3UMtxCalcSIAnmBlendQuat(true)
+	    , mOwner(owner)
+	{
+	}
+
+	virtual void calc(u16);
+
+	/* 0x64 */ TBossWanwan* mOwner;
+};
+
+class TBWBinder : public TBinder {
+public:
+	TBWBinder() { }
+	virtual void bind(TLiveActor*);
+};
+
+class TBWHit : public THitActor {
+public:
+	TBWHit(TBossWanwan* owner, const char* name)
+	    : THitActor(name)
+	    , mOwner(owner)
+	{
+	}
+
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+
+	/* 0x68 */ TBossWanwan* mOwner;
+};
+
+class TBWPicket : public TTakeActor {
+public:
+	TBWPicket(TBossWanwan* owner, const char* name)
+	    : TTakeActor(name)
+	    , mOwner(owner)
+	    , unk74()
+	{
+	}
+
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+	virtual MtxPtr getTakingMtx();
+	virtual bool moveRequest(const JGeometry::TVec3<f32>&);
+
+	/* 0x70 */ TBossWanwan* mOwner;
+	/* 0x74 */ TPosition3f unk74;
+};
+
+class TBWLeashNode : public THitActor {
+public:
+	TBWLeashNode(const char* name)
+	    : THitActor(name)
+	    , unk68(nullptr)
+	    , unk6C(nullptr)
+	    , unk70(0)
+	    , unk74(0.0f)
+	    , unk78(0.0f)
+	{
+	}
+
+	virtual void perform(u32, JDrama::TGraphics*);
+
+	void calcMatrix();
+	void calcTemperature();
+
+	/* 0x68 */ TBossWanwan* unk68;
+	/* 0x6C */ MActor* unk6C;
+	/* 0x70 */ s32 unk70;
+	/* 0x74 */ f32 unk74;
+	/* 0x78 */ f32 unk78;
+	/* 0x7C */ TMtx34f unk7C;
+};
+
+class TBWLeash : public JDrama::TViewObj {
+public:
+	TBWLeash(TBossWanwan*, int, const char*);
+
+	virtual void perform(u32, JDrama::TGraphics*);
+
+	/* 0x10 */ TBossWanwan* mOwner;
+	/* 0x14 */ s32 unk14;
+	/* 0x18 */ TBWLeashNode** unk18;
+	/* 0x1C */ f32 unk1C;
+	/* 0x20 */ f32 unk20;
+};
 
 class TBossWanwan : public TSpineEnemy {
 public:
 	TBossWanwan(const char*);
-	virtual ~TBossWanwan();
 
-	/* 0x150 */ u8 unk150[0x68];
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+	virtual void init(TLiveManager*);
+	virtual void calcRootMatrix();
+	virtual void control();
+	virtual void kill();
+
+	void emitEffects();
+	void slideToCurPathNode(f32, f32);
+	void shakeCamera(int);
+
+	/* 0x150 */ TBossWanwanMtxCalc* mMtxCalc;
+	/* 0x154 */ TBWHit* unk154;
+	/* 0x158 */ TBWPicket* unk158;
+	/* 0x15C */ JGeometry::TVec3<f32> unk15C;
+	/* 0x168 */ f32 unk168;
+	/* 0x16C */ s32 unk16C;
+	/* 0x170 */ TBWLeash* unk170;
+	/* 0x174 */ f32 unk174;
+	/* 0x178 */ f32 unk178;
+	/* 0x17C */ MActor* unk17C;
+	/* 0x180 */ MActor* unk180;
+	/* 0x184 */ MActor* unk184;
+	/* 0x188 */ MActor* unk188;
+	/* 0x18C */ u8 unk18C;
+	/* 0x18D */ u8 unk18D;
+	/* 0x18E */ u8 unk18E[2];
+	/* 0x190 */ s32 unk190;
+	/* 0x194 */ u8 unk194;
+	/* 0x195 */ u8 unk195;
+	/* 0x196 */ u8 unk196[2];
+	/* 0x198 */ s32 unk198;
+	/* 0x19C */ s32 unk19C;
+	/* 0x1A0 */ u8 unk1A0;
+	/* 0x1A1 */ u8 unk1A1[3];
+	/* 0x1A4 */ f32 unk1A4;
+	/* 0x1A8 */ f32 unk1A8;
+	/* 0x1AC */ f32 unk1AC;
+	/* 0x1B0 */ s32 unk1B0;
+	/* 0x1B4 */ u16 unk1B4;
+	/* 0x1B6 */ u8 unk1B6[2];
 };
 
 class TBossWanwanManager : public TEnemyManager {
 public:
 	TBossWanwanManager(const char*);
-	virtual ~TBossWanwanManager();
+
+	virtual void load(JSUMemoryInputStream&);
+	virtual void createModelData();
+	virtual TSpineEnemy* createEnemyInstance();
 };
 
 DECLARE_NERVE(TNerveBWGraphWander, TLiveActor);
--- src/Enemy/bosswanwan.cpp
+++ src/Enemy/bosswanwan.cpp
@@ -1,1 +1,278 @@
+#include <Enemy/BossWanwan.hpp>
+#include <M3DUtil/InfectiousStrings.hpp>
+#include <M3DUtil/MActor.hpp>
+#include <Strategic/ObjModel.hpp>
+#include <Strategic/ObjManager.hpp>
+#include <Strategic/Spine.hpp>
+#include <System/Particles.hpp>
 
+static const char* bwanwan_bastable[] = {
+	"/scene/bwanwan/bas/bwanwan_bark.bas",
+	nullptr,
+	"/scene/bwanwan/bas/bwanwan_shake.bas",
+	nullptr,
+	"/scene/bwanwan/bas/bwanwan_wait.bas",
+	"/scene/bwanwan/bas/bwanwan_wait2.bas",
+	nullptr,
+};
+
+static const TModelDataLoadEntry sModelDataEntries[] = {
+	{ "bwanwan_body.bmd", 0x10220000, 0 },
+	{ "bwanwan_chain.bmd", 0x10220000, 0 },
+	{ "bwanwan_picket.bmd", 0x10220000, 0 },
+	{ nullptr, 0, 0 },
+};
+
+TBWParams::TBWParams(const char* path)
+    : TSpineEnemyParams(path)
+    , PARAM_INIT(mSLMarchSpeed, 6.0f)
+    , PARAM_INIT(mSLTurnSpeed, 1.0f)
+    , PARAM_INIT(mSLLeashNodeLen, 120.0f)
+    , PARAM_INIT(mSLPicketHeight, 100.0f)
+    , PARAM_INIT(mSLPicketRadius, 100.0f)
+    , PARAM_INIT(mSLChainHitHeight, 100.0f)
+    , PARAM_INIT(mSLChainHitRadius, 100.0f)
+    , PARAM_INIT(mSLChainGroundRadius, 60.0f)
+    , PARAM_INIT(mSLPullLimit, 1.0f)
+    , PARAM_INIT(mSLAttackSpeed, 10.0f)
+    , PARAM_INIT(mSLStunTimer, 4000)
+    , PARAM_INIT(mSLSearchLength, 10000.0f)
+    , PARAM_INIT(mSLSearchAngle, 60.0f)
+    , PARAM_INIT(mSLBWHitPointMax, (u8)0xff)
+    , PARAM_INIT(mSLHeadGap, 150.0f)
+    , PARAM_INIT(mSLShakeLengthMax, 3000.0f)
+    , PARAM_INIT(mSLShakeLengthMaxHP0, 2000.0f)
+{
+	TParams::load(mPrmPath);
+}
+
+void TBWLeashNode::calcTemperature() { }
+
+void TBWLeashNode::calcMatrix() { }
+
+void TBWLeashNode::perform(u32 flags, JDrama::TGraphics* graphics)
+{
+	calcTemperature();
+	calcMatrix();
+	THitActor::perform(flags, graphics);
+}
+
+TBWLeash::TBWLeash(TBossWanwan* owner, int node_count, const char* name)
+    : JDrama::TViewObj(name)
+    , mOwner(owner)
+    , unk14(node_count)
+    , unk18(nullptr)
+    , unk1C(0.0f)
+    , unk20(0.0f)
+{
+	unk18 = new TBWLeashNode*[unk14];
+	for (int i = 0; i < unk14; ++i)
+		unk18[i] = new TBWLeashNode("鎖部");
+}
+
+void TBWLeash::perform(u32 flags, JDrama::TGraphics* graphics)
+{
+	for (int i = 0; i < unk14; ++i)
+		unk18[i]->perform(flags, graphics);
+}
+
+BOOL TBWPicket::receiveMessage(THitActor*, u32) { return FALSE; }
+
+bool TBWPicket::moveRequest(const JGeometry::TVec3<f32>& position)
+{
+	mPosition = position;
+	return true;
+}
+
+MtxPtr TBWPicket::getTakingMtx() { return unk74; }
+
+void TBWPicket::perform(u32 flags, JDrama::TGraphics* graphics)
+{
+	THitActor::perform(flags, graphics);
+}
+
+BOOL TBWHit::receiveMessage(THitActor*, u32) { return FALSE; }
+
+void TBWHit::perform(u32 flags, JDrama::TGraphics* graphics)
+{
+	mPosition = mOwner->mPosition;
+	THitActor::perform(flags, graphics);
+}
+
+void TBWBinder::bind(TLiveActor* actor)
+{
+	actor->mPosition.add(actor->mLinearVelocity);
+	actor->mPosition.add(actor->mVelocity);
+}
+
+void TBossWanwanMtxCalc::calc(u16 joint_no)
+{
+	M3UMtxCalcSIAnmBlendQuat::calc(joint_no);
+}
+
+TBossWanwan::TBossWanwan(const char* name)
+    : TSpineEnemy(name)
+    , mMtxCalc(nullptr)
+    , unk154(nullptr)
+    , unk158(nullptr)
+    , unk168(0.0f)
+    , unk16C(0)
+    , unk170(nullptr)
+    , unk17C(nullptr)
+    , unk180(nullptr)
+    , unk184(nullptr)
+    , unk188(nullptr)
+    , unk18C(0)
+    , unk18D(0)
+    , unk190(0)
+    , unk194(1)
+    , unk195(0)
+    , unk198(0)
+    , unk19C(0)
+    , unk1A0(0)
+    , unk1A4(0.0f)
+    , unk1A8(0.0f)
+    , unk1AC(0.0f)
+    , unk1B0(0)
+    , unk1B4(0)
+{
+	mBinder = new TBWBinder;
+}
+
+void TBossWanwan::init(TLiveManager* manager)
+{
+	mManager = manager;
+	manager->manageActor(this);
+
+	mMActorKeeper = new TMActorKeeper(manager, 3);
+	mMActor       = mMActorKeeper->createMActor("bwanwan_body.bmd", 0);
+	mMtxCalc      = new TBossWanwanMtxCalc(this);
+	mMActor->setCalcForBck(mMtxCalc);
+	mMActor->calc();
+
+	unk154 = new TBWHit(this, "ボスワンワンヒット");
+	unk158 = new TBWPicket(this, "ボスワンワンつかみ");
+	unk170 = new TBWLeash(this, 10, "ボスワンワン鎖");
+
+	mSpine->initWith(&TNerveBWGraphWander::theNerve());
+}
+
+void TBossWanwan::shakeCamera(int) { }
+
+BOOL TBossWanwan::receiveMessage(THitActor*, u32) { return FALSE; }
+
+void TBossWanwan::calcRootMatrix() { calcEnemyRootMatrix(); }
+
+void TBossWanwan::slideToCurPathNode(f32 march_speed, f32 turn_speed)
+{
+	walkToCurPathNode(march_speed, turn_speed, 0.0f);
+}
+
+void TBossWanwan::control() { TSpineEnemy::control(); }
+
+void TBossWanwan::emitEffects() { }
+
+void TBossWanwan::perform(u32 flags, JDrama::TGraphics* graphics)
+{
+	if (unk170 != nullptr)
+		unk170->perform(flags, graphics);
+
+	TSpineEnemy::perform(flags, graphics);
+}
+
+void TBossWanwan::kill() { }
+
+TBossWanwanManager::TBossWanwanManager(const char* name)
+    : TEnemyManager(name)
+{
+}
+
+TSpineEnemy* TBossWanwanManager::createEnemyInstance()
+{
+	return new TBossWanwan("ボスワンワン");
+}
+
+void TBossWanwanManager::createModelData() { createModelDataArray(sModelDataEntries); }
+
+void TBossWanwanManager::load(JSUMemoryInputStream& stream)
+{
+	unk38 = new TBWParams("/enemy/bosswanwan.prm");
+
+	TEnemyManager::load(stream);
+
+	SMS_LoadParticle("/scene/bwanwan/jpa/ms_bwan_jump_rock.jpa", 0xad);
+	SMS_LoadParticle("/scene/bwanwan/jpa/ms_bwan_jump_smoke.jpa", 0xae);
+	SMS_LoadParticle("/scene/bwanwan/jpa/ms_bwan_downyuge.jpa", 0xb0);
+	SMS_LoadParticle("/scene/bwanwan/jpa/ms_bwan_hibana.jpa", 0xaf);
+	SMS_LoadParticle("/scene/bwanwan/jpa/ms_bwan_deadyuge.jpa", 0xb1);
+	SMS_LoadParticle("/scene/bwanwan/jpa/ms_bwan_yugami.jpa", 0x1ee);
+	SMS_LoadParticle("/scene/bwanwan/jpa/ms_bwan_hityuge.jpa", 0x167);
+	SMS_LoadParticle("/scene/bwanwan/jpa/ms_bwan_kira.jpa", 0x168);
+}
+
+DEFINE_NERVE(TNerveBWGraphWander, TLiveActor)
+{
+	TBossWanwan* self = (TBossWanwan*)spine->getBody();
+	if (spine->getTime() == 0)
+		self->mMActor->setBck(bwanwan_bastable[4]);
+
+	self->slideToCurPathNode(((TBWParams*)self->getSaveParam())->mSLMarchSpeed.get(),
+	                         ((TBWParams*)self->getSaveParam())->mSLTurnSpeed.get());
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWRoll, TLiveActor)
+{
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWBark, TLiveActor)
+{
+	TBossWanwan* self = (TBossWanwan*)spine->getBody();
+	if (spine->getTime() == 0)
+		self->mMActor->setBck(bwanwan_bastable[0]);
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWJump, TLiveActor)
+{
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWStun, TLiveActor)
+{
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWWakeup, TLiveActor)
+{
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWJumpToBath, TLiveActor)
+{
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWDie, TLiveActor)
+{
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWJumpAway, TLiveActor)
+{
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWShake, TLiveActor)
+{
+	TBossWanwan* self = (TBossWanwan*)spine->getBody();
+	if (spine->getTime() == 0)
+		self->mMActor->setBck(bwanwan_bastable[2]);
+	return false;
+}
+
+DEFINE_NERVE(TNerveBWFall, TLiveActor)
+{
+	return false;
+}
