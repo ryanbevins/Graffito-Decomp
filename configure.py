0666e7cc1a702bebb66f0ae48713ca7a7bfd2994
trader-vps <trader-vps@btc-predictor>
2026-06-13 04:00:26 +0000

    GC2D/ProgSelect: certify Equivalent (links from source, codegen-only diffs)

--- configure.py
+++ configure.py
@@ -1109,7 +1109,7 @@ config.libs = [
             Object(NonMatching, "GC2D/Guide.cpp"),
             Object(Equivalent, "GC2D/SunGlass.cpp"),
             Object(Equivalent, "GC2D/ShineFader.cpp"),
-            Object(NonMatching, "GC2D/ProgSelect.cpp"),
+            Object(Equivalent, "GC2D/ProgSelect.cpp"),
             Object(Equivalent, "GC2D/hx_wiper.c"),
             Object(Equivalent, "GC2D/MovieSubtitle.cpp"),
             Object(NonMatching, "GC2D/Option.cpp"),
