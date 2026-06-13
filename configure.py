2490afc1e26027004bcd120f7dab1c80285dfc65
trader-vps <trader-vps@btc-predictor>
2026-06-13 14:12:26 +0000

    Camera/CubeManagerBase: certify Equivalent (source-link proof)

--- configure.py
+++ configure.py
@@ -1232,7 +1232,7 @@ config.libs = [
             Object(NonMatching, "Camera/lensglow.cpp"),
             Object(Equivalent, "Camera/sunmgr.cpp"),
             Object(Equivalent, "Camera/sunmodel.cpp"),
-            Object(NonMatching, "Camera/CubeManagerBase.cpp"),
+            Object(Equivalent, "Camera/CubeManagerBase.cpp"),
             Object(Equivalent, "Camera/CameraMapTool.cpp"),
             Object(Equivalent, "Camera/CubeMapTool.cpp"),
             Object(Equivalent, "Camera/CameraMultiPlayer.cpp"),
