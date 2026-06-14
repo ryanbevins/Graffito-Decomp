ef0adadb4265fa4f39577e410e948d19249d3fd6
trader-vps <trader-vps@btc-predictor>
2026-06-14 22:32:22 +0000

    Camera/CameraNormal: certify Equivalent (source-link proof)

--- configure.py
+++ configure.py
@@ -1224,7 +1224,7 @@ config.libs = [
             Object(NonMatching, "Camera/cameralib.cpp"),
             Object(Equivalent, "Camera/CameraMarioData.cpp"),
             Object(Equivalent, "Camera/CameraNotice.cpp"),
-            Object(NonMatching, "Camera/CameraNormal.cpp"),
+            Object(Equivalent, "Camera/CameraNormal.cpp"),
             Object(Equivalent, "Camera/camerasave.cpp"),
             Object(Equivalent, "Camera/camerashake.cpp"),
             Object(Equivalent, "Camera/CameraTalk.cpp"),
