c70f9ff7f59cdd824b014e392cecd10596d9f1e5
trader-vps <trader-vps@btc-predictor>
2026-07-11 09:39:49 +0000

    Player/MarioReceiveMsg data: 46.8% -> 50.0% (drop infectious string owners)

--- src/Player/MarioReceiveMsg.cpp
+++ src/Player/MarioReceiveMsg.cpp
@@ -3,7 +3,6 @@
 #undef JGEOMETRY_MARIORECEIVEMSG_TVEC3_SUB_OUT_OF_LINE
 #include <Strategic/HitActor.hpp>
 #include <Strategic/LiveActor.hpp>
-#include <M3DUtil/InfectiousStrings.hpp>
 #include <MSound/MSoundBGM.hpp>
 #include <MSound/MSound.hpp>
 #include <MSound/MSoundSE.hpp>
