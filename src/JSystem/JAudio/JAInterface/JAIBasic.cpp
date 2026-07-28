d445585e96397d83884cf9088c36c7984bff186f
trader-vps <trader-vps@btc-predictor>
2026-07-28 10:42:14 +0000

    JAudio/JAIBasic: loadSceneWave 96.5% -> 99.4% (assign wave at predicate use)

--- src/JSystem/JAudio/JAInterface/JAIBasic.cpp
+++ src/JSystem/JAudio/JAInterface/JAIBasic.cpp
@@ -450,8 +450,8 @@ void JAIBasic::finishSceneSet(u32 param)
 void JAIBasic::loadSceneWave(s32 param1, s32 param2)
 {
 	if (unk54 && unk54[param1].unk8 == 2) {
-		s32 wave = unk60[param1];
-		if (wave != param2) {
+		s32 wave;
+		if (param2 != (wave = unk60[param1])) {
 			if (wave != -1)
 				JASystem::WaveBankMgr::eraseWave(param1, wave);
 			loadGroupWave(param1, param2);
