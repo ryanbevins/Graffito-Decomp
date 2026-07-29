1ae969ed145f0e77ac818f0e8648f546e1d4ab89
trader-vps <trader-vps@btc-predictor>
2026-07-29 00:29:34 +0000

    MoveBG/MapObjBase/startAnim: 95.9% -> 97.63% (clear only active-animation flag)

--- src/MoveBG/MapObjBase.cpp
+++ src/MoveBG/MapObjBase.cpp
@@ -274,10 +274,10 @@ void TMapObjBase::startAnim(u16 param_1)
 		}
 	}
 	if (anim->unk4 != 0) {
-		unkF8 &= ~0x140;
+		unkF8 &= ~0x100;
 		mMActor->setAnimation(anim->unk4, anim->unk8);
 		if (unkF8 & 0x200)
-			unkF8 &= ~0x140;
+			unkF8 &= ~0x100;
 		if (anim->unk10) {
 			setAnmSound(anim->unk10);
 		}
