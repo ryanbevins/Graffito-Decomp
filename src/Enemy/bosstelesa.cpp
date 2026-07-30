01a549fb54048c3bbe0099a15c4a3f5317f43729
trader-vps <trader-vps@btc-predictor>
2026-07-30 20:29:30 +0000

    Enemy/bosstelesa Freeze: 95.2% -> 98.7% (restore alpha clamp)

--- src/Enemy/bosstelesa.cpp
+++ src/Enemy/bosstelesa.cpp
@@ -2057,8 +2057,7 @@ DEFINE_NERVE(TNerveBossTelesaFreeze, TLiveActor)
 			               : 1;
 			u8 alpha = TBossTelesa::mNormalAlpha
 			           + (maxHp - boss->mHitPoints) * 30;
-			if (alpha > 0xFE)
-				alpha = 0xFE;
+			alpha = MsClamp<u8>(alpha, 0, 0xFE);
 
 			boss->unk34C.a = alpha;
 			return TRUE;
