25e56730be007a7a92e7f5427b8597de4dc869ef
trader-vps <trader-vps@btc-predictor>
2026-07-30 13:31:31 +0000

    Enemy/bosstelesa Hide: 90.2% -> 95.5% (retain old-animation actor)

--- src/Enemy/bosstelesa.cpp
+++ src/Enemy/bosstelesa.cpp
@@ -2495,11 +2495,14 @@ DEFINE_NERVE(TNerveBossTelesaHide, TLiveActor)
 		boss->unk160 = 4;
 		boss->unk168 = 1.0f;
 
-		J3DAnmTransform* oldAnm = nullptr;
-		if (boss->mMActor->unkC)
-			oldAnm = boss->mMActor->unkC->unk24;
-		if (boss->mMActor->unkC)
-			boss->mMActor->unkC->setOldMotionBlendAnmPtr(oldAnm);
+		MActor* oldAnmActor = boss->mMActor;
+		J3DAnmTransform* oldAnm;
+		if (!oldAnmActor->unkC)
+			oldAnm = nullptr;
+		else
+			oldAnm = oldAnmActor->unkC->unk24;
+		if (oldAnmActor->unkC)
+			oldAnmActor->unkC->setOldMotionBlendAnmPtr(oldAnm);
 
 		boss->mMActor->setBckFromIndex(4);
 		if (boss->mMActor->unkC)
