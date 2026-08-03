fd35e80f00917e72b289e4881c38c5ed1e6ec4f4
trader-vps <trader-vps@btc-predictor>
2026-08-03 16:17:21 +0000

    MoveBG/MapObjSirena touchWater: 78.7% -> 85.7% (restore positive range branch)

--- src/MoveBG/MapObjSirena.cpp
+++ src/MoveBG/MapObjSirena.cpp
@@ -1010,28 +1010,29 @@ u32 TSlotDrum::touchWater(THitActor* sender)
 {
 	if (unk194 != 0)
 		return 1;
-	if (fabsf(mPosition.x - sender->mPosition.x) >= 150.0f)
-		return 0;
-	f32 hZ = 0.6f * unk140;
-	int dir;
-	if (sender->mPosition.z < mPosition.z - hZ) {
-		dir = 2;
-		if (mRotation.y < 0.0f)
-			dir = 0;
-	} else if (sender->mPosition.z > mPosition.z + hZ) {
-		dir = 0;
-		if (mRotation.y < 0.0f)
+	if (fabsf(mPosition.x - sender->mPosition.x) < 150.0f) {
+		f32 hZ = 0.6f * unk140;
+		int dir;
+		if (sender->mPosition.z < mPosition.z - hZ) {
 			dir = 2;
-	} else {
-		dir = 1;
+			if (mRotation.y < 0.0f)
+				dir = 0;
+		} else if (sender->mPosition.z > mPosition.z + hZ) {
+			dir = 0;
+			if (mRotation.y < 0.0f)
+				dir = 2;
+		} else {
+			dir = 1;
+		}
+		unk164 = 1;
+		f32 sign = (f32)(s16)unk164;
+		unk138[dir] += unk154 * sign;
+		if (fabsf(unk138[dir]) > unk158) {
+			unk138[dir] = unk158 * sign;
+		}
+		return 1;
 	}
-	unk164 = 1;
-	f32 sign = (f32)(s16)unk164;
-	unk138[dir] += unk154 * sign;
-	if (fabsf(unk138[dir]) > unk158) {
-		unk138[dir] = unk158 * sign;
-	}
-	return 1;
+	return 0;
 }
 void TSlotDrum::initMapObj()
 {
