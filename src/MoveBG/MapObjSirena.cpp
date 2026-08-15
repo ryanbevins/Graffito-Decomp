21b64f86b23f56cf1c9e008da533a540189739d1
trader-vps <trader-vps@btc-predictor>
2026-08-15 05:41:27 +0000

    MoveBG/MapObjSirena touchWater: 85.72% -> 100% (recompute clamp sign)

--- src/MoveBG/MapObjSirena.cpp
+++ src/MoveBG/MapObjSirena.cpp
@@ -1029,10 +1029,9 @@ u32 TSlotDrum::touchWater(THitActor* sender)
 			dir = 1;
 		}
 		unk164 = 1;
-		f32 sign = (f32)(s16)unk164;
-		unk138[dir] += unk154 * sign;
+		unk138[dir] += unk154 * (f32)(s16)unk164;
 		if (fabsf(unk138[dir]) > unk158) {
-			unk138[dir] = unk158 * sign;
+			unk138[dir] = unk158 * (f32)(s16)unk164;
 		}
 		return 1;
 	}
