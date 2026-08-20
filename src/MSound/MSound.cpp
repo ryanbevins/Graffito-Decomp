0039960aaccb85e200e7341f2d3eed702d1e6ce0
trader-vps <trader-vps@btc-predictor>
2026-08-20 03:49:14 +0000

    MSound/initSound: 92.68% -> 99.78% (reuse byte min inline)

--- src/MSound/MSound.cpp
+++ src/MSound/MSound.cpp
@@ -519,10 +519,8 @@ void MSound::initSound()
 
 	for (u8 i = 0; i < 16; ++i) {
 		if (MSGMSound->unk0->unk88.unk2[i] && JAIBasic::basic != nullptr) {
-			s32 rawVolume = 127.0f * MSHandle::smSeCategory[i].unk8;
-			s32 categoryVolume
-			    = (u8)rawVolume < 127 ? rawVolume : 127;
-			JAIBasic::basic->setSeCategoryVolume(i, categoryVolume);
+			JAIBasic::basic->setSeCategoryVolume(
+			    i, min<u8>(MSHandle::smSeCategory[i].unk8 * 127.0f, 127));
 		}
 	}
 
