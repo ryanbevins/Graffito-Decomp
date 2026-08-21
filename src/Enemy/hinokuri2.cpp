68b605079517eed98382e3d68e421d18e8ee646c
trader-vps <trader-vps@btc-predictor>
2026-08-21 09:03:06 +0000

    Enemy/hinokuri2 PrePol: restore stamp-probability nerve branch

--- src/Enemy/hinokuri2.cpp
+++ src/Enemy/hinokuri2.cpp
@@ -1222,9 +1222,9 @@ DEFINE_NERVE(TNerveHino2PrePol, TLiveActor)
 			if (uVar5 > wait) {
 				f32 prob = self->getSaveParam()->mSLStampProb.get();
 				if (rand() * (1.0f / (RAND_MAX + 1)) < prob) {
-					spine->pushAfterCurrent(&TNerveHino2Pollute::theNerve());
-				} else {
 					spine->pushAfterCurrent(&TNerveHino2Stamp::theNerve());
+				} else {
+					spine->pushAfterCurrent(&TNerveHino2Pollute::theNerve());
 				}
 
 				self->mWaitTimer = 0;
