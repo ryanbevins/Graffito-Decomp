53c039b67117f55fac709b22af1242881da78445
trader-vps <trader-vps@btc-predictor>
2026-08-23 13:03:35 +0000

    Enemy/BathtubKiller receiveMessage: 58.9% → 62.0% (retain current nerves)

--- src/Enemy/BathtubKiller.cpp
+++ src/Enemy/BathtubKiller.cpp
@@ -646,20 +646,18 @@ void TBathtubKiller::calcRootMatrix()
 BOOL TBathtubKiller::receiveMessage(THitActor* sender, u32 msg)
 {
 	if (msg == 3 || msg <= 1) {
+		const TNerveBase<TLiveActor>* nerve = mSpine->getCurrentNerve();
 		bool isBroken
-		    = mSpine->getCurrentNerve()
-		          == &TNerveBathtubKillerExplosion::theNerve()
-		      || mSpine->getCurrentNerve()
-		             == &TNerveBathtubKillerBreak::theNerve();
+		    = nerve == &TNerveBathtubKillerExplosion::theNerve()
+		      || nerve == &TNerveBathtubKillerBreak::theNerve();
 		if (!isBroken)
 			mSpine->pushNerve(&TNerveBathtubKillerBreak::theNerve());
 		return TRUE;
 	} else if (msg == 0xa) {
+		const TNerveBase<TLiveActor>* nerve = mSpine->getCurrentNerve();
 		bool isBroken
-		    = mSpine->getCurrentNerve()
-		          == &TNerveBathtubKillerExplosion::theNerve()
-		      || mSpine->getCurrentNerve()
-		             == &TNerveBathtubKillerBreak::theNerve();
+		    = nerve == &TNerveBathtubKillerExplosion::theNerve()
+		      || nerve == &TNerveBathtubKillerBreak::theNerve();
 		if (!isBroken)
 			mSpine->pushNerve(&TNerveBathtubKillerExplosion::theNerve());
 		return TRUE;
