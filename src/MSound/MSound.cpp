a1ff12a4b27e5990b82ac1c24594cba84a9025bb
trader-vps <trader-vps@btc-predictor>
2026-08-29 14:16:40 +0000

    MSound/startBeeSe: 95.10% -> 97.93% (invert gate result branch)

--- src/MSound/MSound.cpp
+++ src/MSound/MSound.cpp
@@ -1112,8 +1112,10 @@ u32 MSound::getWallSound(u32 param1, f32 param2)
 void MSound::startBeeSe(Vec* pos, u32 id)
 {
 	if (id > 3) {
-		JAISound* sound = nullptr;
-		if (gateCheck(0x2106)) {
+		JAISound* sound;
+		if (!gateCheck(0x2106)) {
+			sound = nullptr;
+		} else {
 			sound = MSoundSESystem::MSoundSE::startSoundActor(
 			    0x2106, pos, 0, nullptr, 0, 4);
 		}
