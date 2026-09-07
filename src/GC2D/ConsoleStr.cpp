260abd32201cbda3adbd78a4bd2c91cd07307146
trader-vps <trader-vps@btc-predictor>
2026-09-07 21:32:54 +0000

    GC2D/ConsoleStr processShineGet: 90.3% -> 91.06% (retain alpha subtraction)

--- src/GC2D/ConsoleStr.cpp
+++ src/GC2D/ConsoleStr.cpp
@@ -554,7 +554,8 @@ bool TConsoleStr::processShineGet(int param_1)
 		}
 
 		if (param_1 > i * 6 + 200) {
-			s16 alpha = unk244[i]->getPane()->getAlpha() - 7;
+			s16 alpha = unk244[i]->getPane()->getAlpha();
+			alpha -= 7;
 			if (alpha < 0)
 				alpha = 0;
 			unk244[i]->getPane()->setAlpha(alpha);
